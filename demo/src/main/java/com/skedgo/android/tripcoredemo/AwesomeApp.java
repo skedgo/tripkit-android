455cda52ad46df5394cf78053dc1c279796b93e7
Thuy Trinh <thuy@skedgo.com>
2015-07-20 12:50:42 +0700

    Add a basic usage of RouteService

---
+++ demo/src/main/java/com/skedgo/android/tripcoredemo/AwesomeApp.java
@@ -0,0 +1,19 @@
+package com.skedgo.android.tripcoredemo;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.skedgo.android.tripkit.RouteComponent;
+
+public class AwesomeApp extends Application {
+  private RouteComponent routeComponent; // Lazily initialized.
+
+  public synchronized static RouteComponent getRouteComponent(Context context) {
+    final AwesomeApp app = (AwesomeApp) context.getApplicationContext();
+    if (app.routeComponent == null) {
+      app.routeComponent = RouteComponent.create(app);
+    }
+
+    return app.routeComponent;
+  }
+}
--- demo/src/main/java/com/skedgo/android/tripcoredemo/MainFragment.java
+++ demo/src/main/java/com/skedgo/android/tripcoredemo/MainFragment.java
@@ -6,8 +6,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.skedgo.android.common.model.Location;
+import com.skedgo.android.common.model.TripGroup;
+import com.skedgo.android.tripkit.RouteOptions;
+import com.skedgo.android.tripkit.RouteService;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
 public class MainFragment extends Fragment {
   public MainFragment() {
+  }
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    final Location departure = new Location(-33.8599089, 151.2071104);
+    final Location arrival = new Location(-33.876114, 151.205526);
+
+    final RouteOptions routeOptions = new RouteOptions.Builder(departure, arrival)
+        .departAfter(System.currentTimeMillis())
+        .build();
+
+    final RouteService routeService = AwesomeApp.getRouteComponent(getActivity())
+        .provideRouteService();
+    routeService.routeAsync(routeOptions)
+        .subscribeOn(Schedulers.computation())
+        .observeOn(AndroidSchedulers.mainThread())
+        .doOnRequest(new Action1<Long>() {
+          @Override public void call(Long unused) {
+            // Show progress indicator here.
+          }
+        })
+        .doOnTerminate(new Action0() {
+          @Override public void call() {
+            // Dismiss progress indicator here.
+          }
+        })
+        .subscribe(new Action1<TripGroup>() {
+          @Override public void call(TripGroup route) {
+            // Add a newly fetched route into a ListAdapter for example.
+          }
+        }, new Action1<Throwable>() {
+          @Override public void call(Throwable throwable) {
+            // Show error here.
+          }
+        });
   }
 
   @Override
