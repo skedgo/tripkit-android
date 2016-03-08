47f40c8b7117ae3764ab64a144a1aadb29110572
Thuy Trinh <thuy@skedgo.com>
2016-03-08 13:04:13 +0700

    Write some tests to assert defining json properties

---
+++ trip-kit/src/test/java/com/skedgo/android/tripkit/RegionInfoTest.java
@@ -0,0 +1,28 @@
+package com.skedgo.android.tripkit;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.skedgo.android.common.model.ModeInfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class RegionInfoTest {
+  @Test public void jsonProperties() {
+    final RegionInfo regionInfo = ImmutableRegionInfo.builder()
+        .paratransit(new Paratransit())
+        .transitModes(Arrays.asList(new ModeInfo(), new ModeInfo()))
+        .build();
+    final JsonObject json = new Gson().toJsonTree(regionInfo).getAsJsonObject();
+    assertThat(json.has("paratransit")).isTrue();
+    assertThat(json.has("transitModes")).isTrue();
+  }
+}
