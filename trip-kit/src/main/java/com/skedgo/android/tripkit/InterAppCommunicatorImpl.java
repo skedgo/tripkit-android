8f404185d22c0c85837fffdce851cda0fe3ba69d
Emmanuel Lagarrigue Lazarte <emmanuel.lagarrigue@black-tobacco.com>
2016-03-15 13:45:53 -0300

    Add titleForExternalAction() method

--- trip-kit/src/main/java/com/skedgo/android/tripkit/InterAppCommunicatorImpl.java
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/InterAppCommunicatorImpl.java
@@ -1,7 +1,11 @@
 package com.skedgo.android.tripkit;
 
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 import rx.functions.Action1;
 
@@ -24,12 +28,12 @@ public class InterAppCommunicatorImpl implements InterAppCommunicator {
     String urlLink = null;
 
     if (params.action().equals("uber")) {
-      isAppInstalled = isPackageInstalled(packageManager, UBER_PACKAGE);
+      isAppInstalled = deviceHasUber();
       packageId = "uber://";
       urlLink = "https://m.uber.com/sign-up";
     } else if (params.action().startsWith("lyft")) {
       // Also 'lyft_line', etc.
-      isAppInstalled = isPackageInstalled(packageManager, LYFT_PACKAGE);
+      isAppInstalled = deviceHasLyft();
       packageId = "lyft://";
       urlLink = "https://play.google.com/store/apps/details?id=" + LYFT_PACKAGE;
     } else if (params.action().equals("flitways")) {
@@ -46,9 +50,59 @@ public class InterAppCommunicatorImpl implements InterAppCommunicator {
 
   }
 
-  private boolean isPackageInstalled(PackageManager pm, String packageId) {
+  public boolean deviceHasUber() {
+    return isPackageInstalled(UBER_PACKAGE);
+  }
+
+  public boolean deviceHasLyft() {
+    return isPackageInstalled(LYFT_PACKAGE);
+  }
+
+  public String titleForExternalAction(Resources resources, String action) {
+    if (action.equals("gocatch")) {
+      return resources.getString(R.string.gocatch_a_taxi);
+    } else if (action.equals("uber")) {
+      return deviceHasUber()
+          ? resources.getString(R.string.open_uber)
+          : resources.getString(R.string.get_uber);
+
+    } else if (action.startsWith("lyft")) { // also lyft_line, etc.
+      return deviceHasLyft()
+          ? resources.getString(R.string.open_lyft)
+          : resources.getString(R.string.get_lyft);
+
+    } else if (action.equals("flitways")) {
+      return "Book with FlitWays"; // TODO: replace with string resource when available
+
+    } else if (action.startsWith("tel:")) {
+      if (action.contains("name=")) {
+        String name = action.substring(action.indexOf("name=") + "name=".length());
+        try {
+          name = URLDecoder.decode(name, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+          e.printStackTrace();
+        }
+        return resources.getString(R.string.calltaxiformat, name);
+      } else {
+        return resources.getString(R.string.call);
+      }
+
+    } else if (action.startsWith("sms:")) {
+      return "Send SMS"; // TODO: replace with string resource when available
+
+    } else if (action.startsWith("http:") || action.startsWith("https:")) {
+      return resources.getString(R.string.show_website);
+
+    } else {
+      return null;
+    }
+
+    // TODO: handle "ingogo"
+  }
+
+  private boolean isPackageInstalled(String packageId) {
     try {
-      pm.getPackageInfo(packageId, PackageManager.GET_ACTIVITIES);
+      packageManager.getPackageInfo(packageId, PackageManager.GET_ACTIVITIES);
       return true;
     } catch (PackageManager.NameNotFoundException e) {
       // ignored.
