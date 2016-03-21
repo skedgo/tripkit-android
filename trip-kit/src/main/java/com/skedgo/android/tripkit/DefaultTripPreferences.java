def4e0c945123514d5f9af309df56eb692a303d4
Thuy Trinh <thuy@skedgo.com>
2016-03-21 18:08:58 +0700

    Provide default impl of TripPreferences

---
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/DefaultTripPreferences.java
@@ -0,0 +1,32 @@
+package com.skedgo.android.tripkit;
+
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v4.util.ArrayMap;
+
+import java.util.Collections;
+import java.util.Map;
+
+public final class DefaultTripPreferences implements TripPreferences {
+  private final SharedPreferences preferences;
+
+  /**
+   * @param preferences This {@link SharedPreferences} should only be used to store CO2 profile.
+   */
+  public DefaultTripPreferences(@NonNull SharedPreferences preferences) {
+    this.preferences = preferences;
+  }
+
+  @NonNull @Override public Map<String, Float> getCo2Profile() {
+    final Map<String, ?> all = preferences.getAll();
+    final Map<String, Float> co2Profile = new ArrayMap<>(all.size());
+    for (Map.Entry<String, ?> entry : all.entrySet()) {
+      co2Profile.put(entry.getKey(), (Float) entry.getValue());
+    }
+    return Collections.unmodifiableMap(co2Profile);
+  }
+
+  public void setEmissions(@NonNull String modeId, float gramsCO2PerKm) {
+    preferences.edit().putFloat(modeId, gramsCO2PerKm).apply();
+  }
+}
--- trip-kit/src/main/java/com/skedgo/android/tripkit/TripPreferences.java
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/TripPreferences.java
@@ -6,9 +6,15 @@ import java.util.Map;
 
 public interface TripPreferences {
   /**
-   * @return A immutable map having key as mode identifier for
+   * @return An immutable map having key as mode identifier for
    * which to apply emissions, and its value is emissions for
    * the supplied mode identifier in grams of CO2 per kilometre.
    */
   @NonNull Map<String, Float> getCo2Profile();
+
+  /**
+   * @param modeId        Mode identifier for which to apply these emissions.
+   * @param gramsCO2PerKm Emissions for supplied mode identifier in grams of CO2 per kilometre.
+   */
+  void setEmissions(@NonNull String modeId, float gramsCO2PerKm);
 }
