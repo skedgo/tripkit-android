6744f17ec7f47f903ebd62ef91490166d3adae7a
Thuy Trinh <thuy@skedgo.com>
2016-03-25 16:11:23 +0700

    Write tests for creating sms intent from action

--- trip-kit/src/main/java/com/skedgo/android/tripkit/SmsBookingResolver.java
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/SmsBookingResolver.java
@@ -10,24 +10,11 @@ import rx.Observable;
 final class SmsBookingResolver implements BookingResolver {
   public SmsBookingResolver() {}
 
-  @Override public Observable<BookingAction> performExternalActionAsync(ExternalActionParams params) {
-    final BookingAction action = BookingAction.builder()
-        .bookingProvider(BookingResolver.SMS)
-        .hasApp(false)
-        .data(createSmsIntentByAction(params.action()))
-        .build();
-    return Observable.just(action);
-  }
-
-  @Nullable @Override public String getTitleForExternalAction(String externalAction) {
-    return "Send SMS"; // TODO: i18n.
-  }
-
   /**
    * @param uri e.g. 'sms:12345' where '12345' is phone number.
    */
   @NonNull
-  private Intent createSmsIntentByUri(String uri, String smsBody) {
+  static Intent createSmsIntentByUri(String uri, String smsBody) {
     // Put EXTRA_TEXT to avoid missing body on Samsung devices running 5.0.
     return new Intent(Intent.ACTION_VIEW)
         .setData(Uri.parse(uri))
@@ -40,10 +27,23 @@ final class SmsBookingResolver implements BookingResolver {
    *               where '12345' is phone number and 'Body goes here' is sms body.
    */
   @NonNull
-  private Intent createSmsIntentByAction(@NonNull String action) {
+  static Intent createSmsIntentByAction(@NonNull String action) {
     final String[] parts = action.split("\\?");
     final String uri = parts.length > 0 ? parts[0] : null;
     final String smsBody = parts.length > 1 ? parts[1] : null;
     return createSmsIntentByUri(uri, smsBody);
+  }
+
+  @Override public Observable<BookingAction> performExternalActionAsync(ExternalActionParams params) {
+    final BookingAction action = BookingAction.builder()
+        .bookingProvider(BookingResolver.SMS)
+        .hasApp(false)
+        .data(createSmsIntentByAction(params.action()))
+        .build();
+    return Observable.just(action);
+  }
+
+  @Nullable @Override public String getTitleForExternalAction(String externalAction) {
+    return "Send SMS"; // TODO: i18n.
   }
 }
---
+++ trip-kit/src/test/java/com/skedgo/android/tripkit/SmsBookingResolverTest.java
@@ -0,0 +1,36 @@
+package com.skedgo.android.tripkit;
+
+import android.content.Intent;
+import android.net.Uri;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class SmsBookingResolverTest {
+  @Test public void createSmsIntentByActionHavingBothNumberAndBody() {
+    final Intent intent = SmsBookingResolver.createSmsIntentByAction("sms:12345?Hello Android!");
+    assertThat(intent).isNotNull();
+    assertThat(intent.getData()).isEqualTo(Uri.parse("sms:12345"));
+    assertThat(intent.getStringExtra("sms_body")).isEqualTo("Hello Android!");
+  }
+
+  @Test public void createSmsIntentByActionHavingOnlyNumber() {
+    final Intent intent = SmsBookingResolver.createSmsIntentByAction("sms:12345");
+    assertThat(intent).isNotNull();
+    assertThat(intent.getData()).isEqualTo(Uri.parse("sms:12345"));
+    assertThat(intent.getStringExtra("sms_body")).isNull();
+  }
+
+  @Test public void createSmsIntentByActionHavingOnlyNumberAndQuestionMark() {
+    final Intent intent = SmsBookingResolver.createSmsIntentByAction("sms:12345?");
+    assertThat(intent).isNotNull();
+    assertThat(intent.getData()).isEqualTo(Uri.parse("sms:12345"));
+    assertThat(intent.getStringExtra("sms_body")).isNull();
+  }
+}
