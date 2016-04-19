66309ee254d116a5817494c9d5f3941a0f9ac70d
Thuy Trinh <thuy@skedgo.com>
2016-04-19 17:11:32 +0700

    Attempt to define quick booking api

---
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/QuickBookingApi.java
@@ -0,0 +1,13 @@
+package com.skedgo.android.tripkit;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+import rx.Observable;
+
+public interface QuickBookingApi {
+  @GET Observable<List<QuickBooking>> fetchQuickBookingsAsync(
+      @Url String quickBookingsUrl
+  );
+}
