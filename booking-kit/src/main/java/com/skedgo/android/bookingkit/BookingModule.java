3b88b71994c64716dd78b8043ed54631ca447e19
Thuy Trinh <thuy@skedgo.com>
2016-04-27 22:40:08 +0700

    Make BookingKit as an extension of TripKit

--- booking-kit/src/main/java/com/skedgo/android/bookingkit/BookingModule.java
+++ booking-kit/src/main/java/com/skedgo/android/bookingkit/BookingModule.java
@@ -2,10 +2,13 @@ package com.skedgo.android.bookingkit;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.skedgo.android.bookingkit.api.AuthApi;
 import com.skedgo.android.bookingkit.api.BookingApi;
 import com.skedgo.android.bookingkit.api.QuickBookingApi;
 import com.skedgo.android.bookingkit.model.FormField;
 import com.skedgo.android.bookingkit.model.FormFieldJsonAdapter;
+import com.skedgo.android.bookingkit.model.GsonAdaptersAuthProvider;
+import com.skedgo.android.bookingkit.model.GsonAdaptersLogOutResponse;
 import com.skedgo.android.bookingkit.model.GsonAdaptersQuickBooking;
 import com.skedgo.android.bookingkit.viewmodel.AuthenticationViewModel;
 import com.skedgo.android.bookingkit.viewmodel.AuthenticationViewModelImpl;
@@ -22,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import rx.schedulers.Schedulers;
 
 @Module
-public class BookingKitModule {
+public class BookingModule {
   @Provides BookingApi bookingApi(OkHttpClient httpClient) {
     final Gson gson = new GsonBuilder()
         .registerTypeAdapter(FormField.class, new FormFieldJsonAdapter())
@@ -49,6 +52,22 @@ public class BookingKitModule {
         .client(httpClient)
         .build()
         .create(QuickBookingApi.class);
+  }
+
+  @Provides AuthApi authApi(OkHttpClient httpClient) {
+    final Gson gson = new GsonBuilder()
+        .registerTypeAdapter(FormField.class, new FormFieldJsonAdapter())
+        .registerTypeAdapterFactory(new GsonAdaptersAuthProvider())
+        .registerTypeAdapterFactory(new GsonAdaptersLogOutResponse())
+        .create();
+    return new Retrofit.Builder()
+        /* This base url is ignored as the api relies on @Url. */
+        .baseUrl(HttpUrl.parse("https://tripgo.skedgo.com/satapp/"))
+        .addCallAdapterFactory(RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io()))
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(httpClient)
+        .build()
+        .create(AuthApi.class);
   }
 
   @Provides BookingViewModel bookingViewModel(BookingApi bookingApi) {
