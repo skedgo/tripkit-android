f90302a55143fee8d0330858b3234dee20f90272
Thuy Trinh <thuy@skedgo.com>
2016-09-02 16:47:07 +0700

    Implement RealtimeAlertService

--- trip-kit/src/main/java/com/skedgo/android/tripkit/alerts/RealtimeAlertApi.java
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/alerts/RealtimeAlertApi.java
@@ -7,6 +7,9 @@ import retrofit2.http.Query;
 import retrofit2.http.Url;
 import rx.Observable;
 
+/**
+ * Use {@link RealtimeAlertService} for easier usages.
+ */
 public interface RealtimeAlertApi {
   /**
    * See http://skedgo.github.io/tripgo-api/swagger/#!/Transit/get_alerts_transit_json.
