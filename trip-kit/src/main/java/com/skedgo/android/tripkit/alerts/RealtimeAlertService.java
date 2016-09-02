e6942ee263e235cc40f661ec3cdb45c2315681fc
Thuy Trinh <thuy.ntrinh@gmail.com>
2016-09-02 23:28:22 +0700

    Get real-time alerts via `/alerts/transit.json` (#78)
    * Initially implement GET /alerts/transit.json
    
    * Upgrade Mockito to use MockitoRule
    
    * Implement RealtimeAlertService
    
    * Add url for RealtimeAlert
    
    * Add urls

---
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/alerts/RealtimeAlertService.java
@@ -0,0 +1,45 @@
+package com.skedgo.android.tripkit.alerts;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.skedgo.android.common.model.Region;
+
+import java.util.List;
+
+import okhttp3.HttpUrl;
+import retrofit2.http.Query;
+import rx.Observable;
+import rx.functions.Func1;
+
+public class RealtimeAlertService {
+  private final RealtimeAlertApi api;
+
+  public RealtimeAlertService(@NonNull RealtimeAlertApi api) {
+    this.api = api;
+  }
+
+  /**
+   * @param baseUrls   Which can be obtained via {@link Region#getURLs()}.
+   * @param regionName Which can be obtained via {@link Region#getName()}.
+   */
+  public Observable<RealtimeAlertResponse> fetchRealtimeAlertsAsync(
+      @Nullable List<String> baseUrls,
+      @Query("region") final String regionName) {
+    if (baseUrls == null) {
+      return Observable.error(new NullPointerException("baseUrls is null"));
+    } else {
+      return Observable.from(baseUrls)
+          .concatMapDelayError(new Func1<String, Observable<? extends RealtimeAlertResponse>>() {
+            @Override public Observable<? extends RealtimeAlertResponse> call(String baseUrl) {
+              final String url = HttpUrl.parse(baseUrl)
+                  .newBuilder()
+                  .addPathSegments("alerts/transit.json")
+                  .build().toString();
+              return api.fetchRealtimeAlertsAsync(url, regionName);
+            }
+          })
+          .first();
+    }
+  }
+}
