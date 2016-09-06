72014151c41e6ce916731ff787e70d43a2bcc9ef
Thuy Trinh <thuy@skedgo.com>
2016-09-07 06:04:46 +0700

    Add JsonAdapter annotation

--- trip-kit/src/main/java/com/skedgo/android/tripkit/CarPark.java
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/CarPark.java
@@ -1,10 +1,13 @@
 package com.skedgo.android.tripkit;
 
+import com.google.gson.annotations.JsonAdapter;
+
 import org.immutables.gson.Gson;
 import org.immutables.value.Value;
 
 @Gson.TypeAdapters
 @Value.Immutable
+@JsonAdapter(GsonAdaptersCarPark.class)
 public abstract class CarPark {
   public abstract String identifier();
   public abstract String name();
