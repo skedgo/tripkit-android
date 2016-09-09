83c2e6fa80c7f0729df2e3974b332a192b692e27
Thuy Trinh <thuy@skedgo.com>
2016-09-10 03:59:55 +0700

    Write other tests for RegionInfoService

--- trip-kit/src/test/java/com/skedgo/android/tripkit/tsp/RegionInfoServiceTest.java
+++ trip-kit/src/test/java/com/skedgo/android/tripkit/tsp/RegionInfoServiceTest.java
@@ -17,27 +17,35 @@ import java.util.List;
 
 import dagger.Lazy;
 import rx.Observable;
+import rx.exceptions.CompositeException;
 import rx.observers.TestSubscriber;
 
 import static java.util.Collections.singletonList;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class RegionInfoServiceTest {
   @Rule public final MockitoRule rule = MockitoJUnit.rule();
-  @Mock RegionInfoApi regionInfoApi;
+  @Mock RegionInfoApi api;
   private RegionInfoService service;
 
   @Before public void before() {
     service = new RegionInfoService(new Lazy<RegionInfoApi>() {
       @Override public RegionInfoApi get() {
-        return regionInfoApi;
+        return api;
       }
     });
   }
 
+  /**
+   * We manage to fetch via first server, then we ignore second server.
+   */
   @Test public void fetchRegionInfoSuccessfully() {
     final RegionInfo regionInfo = ImmutableRegionInfo.builder()
         .transitWheelchairAccessibility(true)
@@ -45,7 +53,7 @@ public class RegionInfoServiceTest {
     final RegionInfoResponse response = ImmutableRegionInfoResponse.builder()
         .regions(singletonList(regionInfo))
         .build();
-    when(regionInfoApi.fetchRegionInfoAsync(
+    when(api.fetchRegionInfoAsync(
         eq("http://tripgo.com/regionInfo.json"),
         eq(ImmutableRegionInfoBody.of("AU"))
     )).thenReturn(Observable.just(response));
@@ -60,5 +68,65 @@ public class RegionInfoServiceTest {
     subscriber.awaitTerminalEvent();
     subscriber.assertNoErrors();
     subscriber.assertValue(regionInfo);
+  }
+
+  /**
+   * When we fail to fetch via first server but manage via second server.
+   */
+  @Test public void fetchRegionInfoSuccessfullyVia2ndServer() {
+    final RegionInfo regionInfo = ImmutableRegionInfo.builder()
+        .transitWheelchairAccessibility(true)
+        .build();
+    final RegionInfoResponse response = ImmutableRegionInfoResponse.builder()
+        .regions(singletonList(regionInfo))
+        .build();
+    when(api.fetchRegionInfoAsync(
+        eq("http://tripgo.com/regionInfo.json"),
+        eq(ImmutableRegionInfoBody.of("AU"))
+    )).thenReturn(Observable.just(response));
+    final RuntimeException error = new RuntimeException("1st server is down");
+    when(api.fetchRegionInfoAsync(anyString(), any(RegionInfoBody.class)))
+        .thenReturn(Observable.<RegionInfoResponse>error(error))
+        .thenReturn(Observable.just(response));
+
+    final List<String> baseUrls = Arrays.asList(
+        "http://tripgo.com/",
+        "http://riogo.com/"
+    );
+    final TestSubscriber<RegionInfo> subscriber = new TestSubscriber<>();
+    service.fetchRegionInfoAsync(baseUrls, "sydney").subscribe(subscriber);
+    subscriber.awaitTerminalEvent();
+    subscriber.assertNoErrors();
+    subscriber.assertValue(regionInfo);
+
+    verify(api, times(2)).fetchRegionInfoAsync(
+        anyString(),
+        any(RegionInfoBody.class)
+    );
+  }
+
+  /**
+   * When we fail to fetch via both servers.
+   */
+  @Test public void failToFetchRegionInfo() {
+    final RuntimeException firstError = new RuntimeException("1st server is down");
+    final RuntimeException secondError = new RuntimeException("2nd server is down");
+    when(api.fetchRegionInfoAsync(anyString(), any(RegionInfoBody.class)))
+        .thenReturn(Observable.<RegionInfoResponse>error(firstError))
+        .thenReturn(Observable.<RegionInfoResponse>error(secondError));
+
+    final List<String> baseUrls = Arrays.asList(
+        "http://tripgo.com/",
+        "http://riogo.com/"
+    );
+    final TestSubscriber<RegionInfo> subscriber = new TestSubscriber<>();
+    service.fetchRegionInfoAsync(baseUrls, "sydney").subscribe(subscriber);
+    subscriber.awaitTerminalEvent();
+    subscriber.assertError(CompositeException.class);
+
+    verify(api, times(2)).fetchRegionInfoAsync(
+        anyString(),
+        any(RegionInfoBody.class)
+    );
   }
 }
