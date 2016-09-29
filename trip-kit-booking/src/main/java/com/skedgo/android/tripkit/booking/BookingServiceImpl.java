af914c2dcde0be400043d871ebf31cdcabea3835
Emmanuel Lagarrigue Lazarte <emmanuel.lagarrigue@black-tobacco.com>
2016-09-29 15:13:06 -0300

    Fix no message booking error issue (#86)
    * Fix no message booking error issue
    
    * Remove unnecessary JsonReader and name refactor

--- trip-kit-booking/src/main/java/com/skedgo/android/tripkit/booking/BookingServiceImpl.java
+++ trip-kit-booking/src/main/java/com/skedgo/android/tripkit/booking/BookingServiceImpl.java
@@ -1,22 +1,21 @@
 package com.skedgo.android.tripkit.booking;
 
+import android.support.annotation.VisibleForTesting;
+
 import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
 
 import java.io.IOException;
-import java.io.StringReader;
 
 import retrofit2.Response;
 import rx.Observable;
 import rx.Single;
-import rx.functions.Func0;
 import rx.functions.Func1;
 
 public class BookingServiceImpl implements BookingService {
 
   private final BookingApi bookingApi;
   private final ExternalOAuthStore externalOAuthStore;
-  private final  Gson gson;
+  private final Gson gson;
 
   public BookingServiceImpl(BookingApi bookingApi, ExternalOAuthStore externalOAuthStore, Gson gson) {
     this.bookingApi = bookingApi;
@@ -32,10 +31,13 @@ public class BookingServiceImpl implements BookingService {
 
     return bookingApi.postFormAsync(url, inputForm)
         .flatMap(new Func1<Response<BookingForm>, Observable<BookingForm>>() {
-          @Override public Observable<BookingForm> call(final Response<BookingForm> bookingFormResponse) {
+          @Override
+          public Observable<BookingForm> call(final Response<BookingForm> bookingFormResponse) {
+
             if (!bookingFormResponse.isSuccessful()) {
               try {
-                return Observable.error(asBookingError(bookingFormResponse.errorBody().string()));
+                BookingError e = asBookingError(bookingFormResponse.errorBody().string());
+                return Observable.error(e);
               } catch (IOException e) {
                 e.printStackTrace();
               }
@@ -49,10 +51,8 @@ public class BookingServiceImpl implements BookingService {
     return externalOAuthStore.getExternalOauth(authId);
   }
 
-
-  private BookingError asBookingError(String bookingErrorJson) {
-    final JsonReader jsonReader = new JsonReader(new StringReader(bookingErrorJson));
-    return gson.fromJson(jsonReader, BookingError.class);
+  @VisibleForTesting BookingError asBookingError(String bookingErrorJson) {
+    return gson.fromJson(bookingErrorJson, BookingError.class);
   }
 
 }
