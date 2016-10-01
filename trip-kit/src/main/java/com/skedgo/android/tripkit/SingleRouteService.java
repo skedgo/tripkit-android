7eb168d1dbaf04d4f561782142fd338e3ccaa276
Thuy Trinh <thuy@skedgo.com>
2016-10-01 21:40:29 +0700

    Implement SingleRouteService

---
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/SingleRouteService.java
@@ -0,0 +1,44 @@
+package com.skedgo.android.tripkit;
+
+import android.support.annotation.NonNull;
+
+import com.skedgo.android.common.model.Query;
+import com.skedgo.android.common.model.TripGroup;
+
+import java.util.List;
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+import rx.subjects.Subject;
+
+/**
+ * A decorator of {@link RouteService} that performs only one routing request.
+ * For example, if we ask it to route from A to B, and while that request
+ * is still progress and later we ask it to route from C to B,
+ * then the request A-to-B will be cancelled.
+ * Cancellation is invoked asynchronously. That means the execution
+ * of the request C-to-D doesn't have to wait for
+ * the cancellation of the request A-to-B to be done to get started.
+ */
+public class SingleRouteService implements RouteService {
+  /* toSerialized() to be thread-safe. */
+  private final Subject<Void, Void> cancellationSignal =
+      PublishSubject.<Void>create().toSerialized();
+  private final RouteService routeService;
+
+  public SingleRouteService(RouteService routeService) {
+    this.routeService = routeService;
+  }
+
+  @NonNull @Override public Observable<TripGroup> routeAsync(@NonNull RouteOptions options) {
+    cancellationSignal.onNext(null);
+    return routeService.routeAsync(options)
+        .takeUntil(cancellationSignal.asObservable());
+  }
+
+  @NonNull @Override public Observable<List<TripGroup>> routeAsync(@NonNull Query query) {
+    cancellationSignal.onNext(null);
+    return routeService.routeAsync(query)
+        .takeUntil(cancellationSignal.asObservable());
+  }
+}
