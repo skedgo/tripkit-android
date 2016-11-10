4cd84265231092c74262c407e6c256bdeb0ccc38
Thuy Trinh <thuy@skedgo.com>
2016-11-10 17:07:33 +0700

    Migrate Street to immutable

--- android-shared-lib/src/main/java/com/skedgo/android/common/model/Street.java
+++ android-shared-lib/src/main/java/com/skedgo/android/common/model/Street.java
@@ -6,128 +6,52 @@ package com.skedgo.android.common.model;
 
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.text.TextUtils;
+import android.support.annotation.Nullable;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.google.gson.annotations.SerializedName;
-import com.skedgo.android.common.util.LogUtils;
-import com.skedgo.android.common.util.PolylineEncoderUtils;
+import com.google.gson.annotations.JsonAdapter;
 
-import java.util.LinkedList;
-import java.util.List;
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
 
-/**
- * Created: 19/02/13 11:52 AM
- */
-public class Street implements Parcelable {
-  public static final String TAG = LogUtils.makeTag(Street.class);
+@Value.Immutable
+@Gson.TypeAdapters
+@JsonAdapter(GsonAdaptersStreet.class)
+public abstract class Street implements Parcelable {
   public static final Creator<Street> CREATOR = new Creator<Street>() {
-    public Street createFromParcel(Parcel in) {
-      Street s = new Street();
-
-      s.mId = in.readLong();
-      s.mName = in.readString();
-      s.mMeters = in.readFloat();
-      s.mWayPoints = in.readArrayList(Double.class.getClassLoader());
-      s.mWaypointEncoding = in.readString();
-      s.safe = in.readByte() == 1;
-      return s;
+    @Override public Street createFromParcel(Parcel in) {
+      return ImmutableStreet.builder()
+          .name(in.readString())
+          .meters(in.readFloat())
+          .encodedWaypoints(in.readString())
+          .safe(in.readByte() == 1)
+          .build();
     }
 
-    public Street[] newArray(int size) {
+    @Override public Street[] newArray(int size) {
       return new Street[size];
     }
   };
-  private transient long mId;
-  @SerializedName("name")
-  private String mName;
-  @SerializedName("metres")
-  private float mMeters;
-  @SerializedName("waypoints")
-  private List<Double> mWayPoints;
-  private boolean safe;
-  /**
-   * Save encoding .. we will lazily decode it..
-   * <p/>
-   * NOTE: As of v3 of the TripGO API, the server does the work for us!
-   */
-  @SerializedName("encodedWaypoints")
-  private String mWaypointEncoding;
 
-  public long getId() {
-    return mId;
+  public abstract @Nullable String name();
+  public abstract @Nullable String encodedWaypoints();
+
+  @Value.Default public float meters() {
+    return 0f;
   }
 
-  public void setId(final long id) {
-    mId = id;
+  @Value.Default public boolean safe() {
+    return false;
   }
 
-  public String getName() {
-    return mName;
-  }
-
-  public void setName(final String name) {
-    this.mName = name;
-  }
-
-  public float getMeters() {
-    return mMeters;
-  }
-
-  public void setMetres(final float meters) {
-    this.mMeters = meters;
-  }
-
-  public List<Double> getWayPoints() {
-    if ((mWayPoints == null || mWayPoints.isEmpty()) && !TextUtils.isEmpty(mWaypointEncoding)) {
-      List<LatLng> coords = PolylineEncoderUtils.decode(mWaypointEncoding);
-
-      if (coords != null) {
-        List<Double> wps = new LinkedList<Double>();
-        for (LatLng c : coords) {
-          wps.add(c.latitude);
-          wps.add(c.longitude);
-        }
-
-        setWayPoints(wps);
-      }
-    }
-
-    return mWayPoints;
-  }
-
-  public void setWayPoints(List<Double> wps) {
-    mWayPoints = wps;
-  }
-
-  public String getWaypointEncoding() {
-    return mWaypointEncoding;
-  }
-
-  public void setWaypointEncoding(String waypointEncoding) {
-    mWaypointEncoding = waypointEncoding;
-  }
-
-  @Override
-  public int describeContents() {
-    return hashCode();
+  @Override public int describeContents() {
+    return 0;
   }
 
   @Override
   public void writeToParcel(final Parcel dest, final int flags) {
-    dest.writeLong(mId);
-    dest.writeString(mName);
-    dest.writeFloat(mMeters);
-    dest.writeList(mWayPoints);
-    dest.writeString(mWaypointEncoding);
-    dest.writeByte((byte) (safe ? 1 : 0));
-  }
-
-  public boolean isSafe() {
-    return safe;
-  }
-
-  public void setSafe(boolean safe) {
-    this.safe = safe;
+    dest.writeString(name());
+    dest.writeFloat(meters());
+    dest.writeString(encodedWaypoints());
+    dest.writeByte((byte) (safe() ? 1 : 0));
   }
 }
