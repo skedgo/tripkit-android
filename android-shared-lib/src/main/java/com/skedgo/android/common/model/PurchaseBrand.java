1f01e5b3631d666b5fcbd13410183630185bbebc
Emmanuel Lagarrigue <emmanuel.lagarrigue@black-tobacco.com>
2016-12-01 14:49:07 -0300

    Add booking purchase brand object

---
+++ android-shared-lib/src/main/java/com/skedgo/android/common/model/PurchaseBrand.java
@@ -0,0 +1,38 @@
+package com.skedgo.android.common.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.JsonAdapter;
+
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
+
+@Gson.TypeAdapters
+@Value.Immutable
+@JsonAdapter(GsonAdaptersPurchaseBrand.class)
+public abstract class PurchaseBrand implements Parcelable {
+
+  public static final Creator<PurchaseBrand> CREATOR = new Creator<PurchaseBrand>() {
+    @Override public PurchaseBrand createFromParcel(Parcel in) {
+      return ImmutablePurchaseBrand.builder()
+          .color((ServiceColor) in.readParcelable(ServiceColor.class.getClassLoader()))
+          .build();
+    }
+
+    @Override public PurchaseBrand[] newArray(int size) {
+      return new PurchaseBrand[size];
+    }
+  };
+
+  @Override public void writeToParcel(Parcel dest, int flags) {
+    dest.writeParcelable(color(), flags);
+  }
+
+  @Override public int describeContents() {
+    return 0;
+  }
+
+  public abstract ServiceColor color();
+
+}
