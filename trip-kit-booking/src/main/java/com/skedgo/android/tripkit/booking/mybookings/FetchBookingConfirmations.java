596958208f0229f5fa1da7b12b9a422f1bee173b
Thuy Trinh <thuy@skedgo.com>
2016-12-28 16:13:04 +0700

    Rename into a type of use-case

--- trip-kit-booking/src/main/java/com/skedgo/android/tripkit/booking/mybookings/FetchBookingConfirmations.java
+++ trip-kit-booking/src/main/java/com/skedgo/android/tripkit/booking/mybookings/FetchBookingConfirmations.java
@@ -7,14 +7,14 @@ import java.util.List;
 import rx.Observable;
 import rx.functions.Func1;
 
-public class MyBookingsService {
+public class FetchBookingConfirmations {
   private final MyBookingsApi api;
 
-  public MyBookingsService(MyBookingsApi api) {
+  public FetchBookingConfirmations(MyBookingsApi api) {
     this.api = api;
   }
 
-  public Observable<List<BookingConfirmation>> getMyBookingsAsync(int first, int pageSize) {
+  public Observable<List<BookingConfirmation>> call(int first, int pageSize) {
     return api.fetchMyBookingsAsync(first, pageSize)
         .map(new Func1<MyBookingsResponse, List<BookingConfirmation>>() {
           @Override public List<BookingConfirmation> call(MyBookingsResponse response) {
