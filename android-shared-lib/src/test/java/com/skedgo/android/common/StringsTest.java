3bf80ce8518da7fb7b12e1d502d950d14f644996
Emmanuel Lagarrigue <emmanuel.lagarrigue@black-tobacco.com>
2017-01-12 13:50:45 -0300

    Update generated strings

--- android-shared-lib/src/test/java/com/skedgo/android/common/StringsTest.java
+++ android-shared-lib/src/test/java/com/skedgo/android/common/StringsTest.java
@@ -19,17 +19,17 @@ public class StringsTest {
    * https://www.flowdock.com/app/skedgo/androiddev/threads/Q4zIifXMtFIOZPHGanuNCLZobjE.
    */
   @Test public void someEnglishStringsHaveCorrectValues() {
-    assertThat(resources.getString(R.string.viewpricebutton)).isEqualTo("Price");
-    assertThat(resources.getString(R.string.viewdurationbutton)).isEqualTo("Duration");
-    assertThat(resources.getString(R.string.viewoverallbutton)).isEqualTo("Preferred");
-    assertThat(resources.getString(R.string.namevehicle)).isEqualTo("Name");
-    assertThat(resources.getString(R.string.leaveafter)).isEqualTo("Leave at");
-    assertThat(resources.getString(R.string.arrivebefore)).isEqualTo("Arrive by");
-    assertThat(resources.getString(R.string.titlefavouritename)).isEqualTo("Title");
-    assertThat(resources.getString(R.string.newitemhereactiontitle)).isEqualTo("Add when I'm here");
-    assertThat(resources.getString(R.string.locationwanttogo)).isEqualTo("Where do you want to go?");
-    assertThat(resources.getString(R.string.dropnewpin)).isEqualTo("Drop new pin");
-    assertThat(resources.getString(R.string.setlocation)).isEqualTo("Tap to set location");
+    assertThat(resources.getString(R.string.price)).isEqualTo("Price");
+    assertThat(resources.getString(R.string.duration)).isEqualTo("Duration");
+    assertThat(resources.getString(R.string.preferred)).isEqualTo("Preferred");
+    assertThat(resources.getString(R.string.name)).isEqualTo("Name");
+    assertThat(resources.getString(R.string.leave_at)).isEqualTo("Leave at");
+    assertThat(resources.getString(R.string.arrive_by)).isEqualTo("Arrive by");
+    assertThat(resources.getString(R.string.title)).isEqualTo("Title");
+    assertThat(resources.getString(R.string.add_when_i_apostm_here)).isEqualTo("Add when I'm here");
+    assertThat(resources.getString(R.string.where_do_you_want_to_go_question)).isEqualTo("Where do you want to go?");
+    assertThat(resources.getString(R.string.drop_new_pin)).isEqualTo("Drop new pin");
+    assertThat(resources.getString(R.string.tap_to_set_location)).isEqualTo("Tap to set location");
   }
 
   @Test
@@ -42,13 +42,13 @@ public class StringsTest {
   @Test
   @Config(qualifiers = "pt")
   public void correctFormatOfXMinutesInPortuguese() {
-    final String minutes = resources.getString(R.string.minutes);
+    final String minutes = resources.getString(R.string._perc_dot0f_minutes);
     assertThat(String.format(minutes, 2f))
         .isEqualTo("2 minutos");
   }
 
   @Test public void correctFormatOfXMinutesInEnglish() {
-    final String minutes = resources.getString(R.string.minutes);
+    final String minutes = resources.getString(R.string._perc_dot0f_minutes);
     assertThat(String.format(minutes, 2f))
         .isEqualTo("2 minutes");
   }
@@ -65,47 +65,42 @@ public class StringsTest {
   }
 
   @Test public void shouldHaveCorrectStringForDepartureLocationHint() {
-    assertThat(resources.getString(R.string.locationcomingfrom))
+    assertThat(resources.getString(R.string.where_are_you_going_from_question))
         .isEqualTo("Where are you going from?");
   }
 
   @Test public void shouldHaveCorrectStringForEventName() {
-    assertThat(resources.getString(R.string.titleeventname))
+    assertThat(resources.getString(R.string.title))
         .isEqualTo("Title");
   }
 
   @Test public void shouldHaveCorrectStringForStartDateTime() {
-    assertThat(resources.getString(R.string.eventstartdatetime))
+    assertThat(resources.getString(R.string.start))
         .isEqualTo("Start");
   }
 
   @Test public void shouldHaveCorrectStringForEndDateTime() {
-    assertThat(resources.getString(R.string.eventenddatetime))
-        .isEqualTo("End");
+    assertThat(resources.getString(R.string.end_date))
+        .isEqualTo("End Date");
   }
 
   @Test public void shouldHaveCorrectStringForRepeats() {
-    assertThat(resources.getString(R.string.eventrepeat))
+    assertThat(resources.getString(R.string.repeats))
         .isEqualTo("Repeats");
   }
 
   @Test public void shouldHaveCorrectStringForAgendaButton() {
-    assertThat(resources.getString(R.string.agendabutton))
+    assertThat(resources.getString(R.string.agenda))
         .isEqualTo("Agenda");
   }
 
   @Test public void shouldHaveCorrectStringForAirlineFlightNumber() {
-    assertThat(resources.getString(R.string.airlineflightnumber))
+    assertThat(resources.getString(R.string._pattern_flight__pattern))
         .isEqualTo("%1$s flight %2$s");
   }
 
-  @Test public void shouldHaveCorrectStringForAlertTitleFormat() {
-    assertThat(resources.getString(R.string.alerttitleformat))
-        .isEqualTo("Alert: %1$s");
-  }
-
   @Test public void shouldHaveCorrectStringForBookTaxi() {
-    assertThat(resources.getString(R.string.booktaxi))
+    assertThat(resources.getString(R.string.book_a_taxi))
         .isEqualTo("Book a taxi");
   }
 
@@ -115,58 +110,33 @@ public class StringsTest {
   }
 
   @Test public void shouldHaveCorrectStringForCalendarEventDescription() {
-    assertThat(resources.getString(R.string.calendareventdescription))
+    assertThat(resources.getString(R.string.regular_events_which_will_be_saved_in_your_device_aposts_calendar_dot))
         .isEqualTo("Regular events which will be saved in your device's calendar.");
   }
 
   @Test public void shouldHaveCorrectStringForCriticalError() {
-    assertThat(resources.getString(R.string.criticalerror))
+    assertThat(resources.getString(R.string.critical_error))
         .isEqualTo("Critical Error");
   }
 
   @Test public void shouldHaveCorrectStringForCriticalErrorFormat() {
-    assertThat(resources.getString(R.string.criticalerrorformat))
+    assertThat(resources.getString(R.string.the_application_encountered_a_critical_error_and_can_apostt_recover_2points))
         .isEqualTo("The application encountered a critical error and can't recover:");
   }
 
   @Test public void shouldHaveCorrectStringForDateFromFormat() {
-    assertThat(resources.getString(R.string.datefromformat))
+    assertThat(resources.getString(R.string.from__pattern))
         .isEqualTo("From %1$s");
   }
 
-  @Test public void shouldHaveCorrectStringForEditItemHereTitle() {
-    assertThat(resources.getString(R.string.edititemheretitle))
-        .isEqualTo("Agenda Item");
-  }
-
-  @Test public void shouldHaveCorrectStringForExcuseDueToLate() {
-    assertThat(resources.getString(R.string.excuseduetolate))
-        .isEqualTo("TripGo - There goes my excuse for being late...");
-  }
-
   @Test public void shouldHaveCorrectStringForFavouritesButton() {
-    assertThat(resources.getString(R.string.favouritesbutton))
+    assertThat(resources.getString(R.string.favourites))
         .isEqualTo("Favourites");
   }
 
   @Test public void shouldHaveCorrectStringForFlightNumberFormat() {
-    assertThat(resources.getString(R.string.flightnumberformat))
-        .isEqualTo("Flight %1$s");
-  }
-
-  @Test public void shouldHaveCorrectStringForFreeDirections() {
-    assertThat(resources.getString(R.string.freedirections))
-        .isEqualTo("TripGo - Free directions");
-  }
-
-  @Test public void shouldHaveCorrectStringForFromToForEventLocationFormat() {
-    assertThat(resources.getString(R.string.fromtoforeventlocationformat))
-        .isEqualTo("From %1$s to %2$s");
-  }
-
-  @Test public void shouldHaveCorrectStringForGetAround() {
-    assertThat(resources.getString(R.string.getaround))
-        .isEqualTo("I use TripGo to get around.");
+    assertThat(resources.getString(R.string._pattern_flight__pattern))
+        .isEqualTo("%1$s flight %2$s");
   }
 
   @Test public void shouldHaveCorrectStringForHabitual_Visit() {
@@ -175,87 +145,57 @@ public class StringsTest {
   }
 
   @Test public void shouldHaveCorrectStringForHabitualEventDescription() {
-    assertThat(resources.getString(R.string.habitualeventdescription))
+    assertThat(resources.getString(R.string.routines_are_part_of_your_daily_movement_coma_but_they_aren_apostt_events_that_you_want_in_your_calendar__start_parente_dotg_dot_coma_work_end_parent_dot_they_are_important_for_the_agenda_to_get_you_to_the_right_location_at_the_right_time_dot))
         .isEqualTo("Routines are part of your daily movement, but they aren't events that you want in your calendar (e.g., work). They are important for the Agenda to get you to the right location at the right time.");
   }
 
   @Test public void shouldHaveCorrectStringForLocationComingFrom() {
-    assertThat(resources.getString(R.string.locationcomingfrom))
+    assertThat(resources.getString(R.string.where_are_you_going_from_question))
         .isEqualTo("Where are you going from?");
   }
 
-  @Test public void shouldHaveCorrectStringForLostOnce() {
-    assertThat(resources.getString(R.string.lostonce))
-        .isEqualTo("I once was lost but now I have found... TripGo.");
-  }
-
   @Test public void shouldHaveCorrectStringForMinutesBefore() {
-    assertThat(resources.getString(R.string.minutesbefore))
+    assertThat(resources.getString(R.string._perc_dot0f_minutes_before))
         .isEqualTo("%.0f minutes before");
   }
 
   @Test public void shouldHaveCorrectStringForNewEvent() {
-    assertThat(resources.getString(R.string.newevent))
-        .isEqualTo("New Event");
+    assertThat(resources.getString(R.string.new_event))
+        .isEqualTo("New event");
   }
 
   @Test public void shouldHaveCorrectStringForNewItemAwayActionTitle() {
-    assertThat(resources.getString(R.string.newitemawayactiontitle))
+    assertThat(resources.getString(R.string.stay_at_a_different_location))
         .isEqualTo("Stay at a different location");
   }
 
-  @Test public void shouldHaveCorrectStringForNewStayTitle() {
-    assertThat(resources.getString(R.string.newstaytitle))
-        .isEqualTo("New Stay");
-  }
-
-  @Test public void shouldHaveCorrectStringForNumberRouteFormat() {
-    assertThat(resources.getString(R.string.numberrouteformat))
-        .isEqualTo("Route %1$s of %2$s");
-  }
-
   @Test public void shouldHaveCorrectStringForSearchButton() {
-    assertThat(resources.getString(R.string.searchbutton))
+    assertThat(resources.getString(R.string.search))
         .isEqualTo("Search");
   }
 
   @Test public void shouldHaveCorrectStringForTitleEventName() {
-    assertThat(resources.getString(R.string.titleeventname))
+    assertThat(resources.getString(R.string.title))
         .isEqualTo("Title");
   }
 
-  @Test public void shouldHaveCorrectStringForTransportGenius() {
-    assertThat(resources.getString(R.string.transportgenius))
-        .isEqualTo("TripGo - Transport genius");
-  }
-
-  @Test public void shouldHaveCorrectStringForTransportModeWish() {
-    assertThat(resources.getString(R.string.transportmodewish))
-        .isEqualTo("TripGo - How do you want to go today?");
-  }
-
   @Test public void shouldHaveCorrectStringForTripsButton() {
-    assertThat(resources.getString(R.string.tripsbutton))
+    assertThat(resources.getString(R.string.trips))
         .isEqualTo("Trips");
   }
 
-  @Test public void shouldHaveCorrectStringForTypeEvent() {
-    assertThat(resources.getString(R.string.typeevent))
-        .isEqualTo("Type");
-  }
-
-  @Test public void shouldHaveCorrectStringForTypeVehicle() {
-    assertThat(resources.getString(R.string.typevehicle))
+  @Test public void shouldHaveCorrectStringForType() {
+    assertThat(resources.getString(R.string.type))
         .isEqualTo("Type");
   }
 
   @Test public void shouldHaveCorrectStringForWhenIAmAwayHeader() {
-    assertThat(resources.getString(R.string.wheniamawayheader))
-        .isEqualTo("Stays");
+    assertThat(resources.getString(R.string.stay))
+        .isEqualTo("Stay");
   }
 
   @Test public void shouldHaveCorrectStringForWhenIAmHereHeader() {
-    assertThat(resources.getString(R.string.wheniamhereheader))
+    assertThat(resources.getString(R.string.agenda))
         .isEqualTo("Agenda");
   }
 
