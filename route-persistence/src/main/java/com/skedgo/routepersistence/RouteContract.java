610bab52c4c7a3d818c618859f29d22c29de0475
Thuy Trinh <thuy@skedgo.com>
2017-01-16 12:54:05 +0000

    Put `route-persistence` code

---
+++ route-persistence/src/main/java/com/skedgo/routepersistence/RouteContract.java
@@ -0,0 +1,100 @@
+package com.skedgo.routepersistence;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import skedgo.sqlite.DatabaseField;
+import skedgo.sqlite.DatabaseTable;
+import skedgo.sqlite.UniqueIndices;
+
+final class RouteContract {
+  static final String COL_REQUEST_ID = "requestId";
+  static final String COL_UUID = "uuid";
+  static final String COL_GROUP_ID = "groupId";
+  static final String COL_FREQUENCY = "frequency";
+  static final String COL_DISPLAY_TRIP_ID = "displayTripId";
+  static final String COL_ID = "id";
+  static final String COL_CURRENCY_SYMBOL = "currencySymbol";
+  static final String COL_SAVE_URL = "saveURL";
+  static final String COL_UPDATE_URL = "updateURL";
+  static final String COL_PROGRESS_URL = "progressURL";
+  static final String COL_PLANNED_URL = "plannedURL";
+  static final String COL_TEMP_URL = "temporaryURL";
+  static final String COL_DEPART = "depart";
+  static final String COL_ARRIVE = "arrive";
+  static final String COL_QUERY_IS_LEAVE_AFTER = "queryIsLeaveAfter";
+  static final String COL_CALORIES_COST = "caloriesCost";
+  static final String COL_MONEY_COST = "moneyCost";
+  static final String COL_CARBON_COST = "carbonCost";
+  static final String COL_HASSLE_COST = "hassleCost";
+  static final String COL_WEIGHTED_SCORE = "weightedScore";
+  static final String COL_TRIP_ID = "tripId";
+  static final String COL_JSON = "json";
+  static final String TABLE_SEGMENTS = "segments";
+  static final String SELECT_SEGMENTS = "select * from " + TABLE_SEGMENTS + " where tripId = ?";
+  static final String TABLE_TRIPS = "trips";
+  static final String SELECT_TRIPS = "select * from " + TABLE_TRIPS + " where " + COL_GROUP_ID + " = ?";
+  static final String TABLE_TRIP_GROUPS = "tripGroups";
+  static final String COL_IS_NOTIFIABLE = "isNotifiable";
+
+  private RouteContract() {}
+
+  static void createTables(SQLiteDatabase database) {
+    final DatabaseField frequency = new DatabaseField(COL_FREQUENCY, "integer");
+    final DatabaseField displayTripId = new DatabaseField(COL_DISPLAY_TRIP_ID, "integer");
+    final DatabaseField depart = new DatabaseField(COL_DEPART, "integer");
+    final DatabaseField arrive = new DatabaseField(COL_ARRIVE, "integer");
+    final DatabaseField queryIsLeaveAfter = new DatabaseField(COL_QUERY_IS_LEAVE_AFTER, "integer");
+    final DatabaseField caloriesCost = new DatabaseField(COL_CALORIES_COST, "real");
+    final DatabaseField tempUrl = new DatabaseField(COL_TEMP_URL, "text");
+    final DatabaseField plannedUrl = new DatabaseField(COL_PLANNED_URL, "text");
+    final DatabaseField progressUrl = new DatabaseField(COL_PROGRESS_URL, "text");
+    final DatabaseField moneyCost = new DatabaseField(COL_MONEY_COST, "real");
+    final DatabaseField carbonCost = new DatabaseField(COL_CARBON_COST, "real");
+    final DatabaseField updateUrl = new DatabaseField(COL_UPDATE_URL, "text");
+    final DatabaseField saveUrl = new DatabaseField(COL_SAVE_URL, "text");
+    final DatabaseField currencySymbol = new DatabaseField(COL_CURRENCY_SYMBOL, "text");
+    final DatabaseField hassleCost = new DatabaseField(COL_HASSLE_COST, "real");
+    final DatabaseField weightedScore = new DatabaseField(COL_WEIGHTED_SCORE, "real");
+    final DatabaseField tripId = new DatabaseField(COL_TRIP_ID, "text");
+    final DatabaseField _id = new DatabaseField("_id", "integer", "primary key autoincrement");
+    final DatabaseField id = new DatabaseField(COL_ID, "integer");
+    final DatabaseField groupId = new DatabaseField(COL_GROUP_ID, "text");
+    final DatabaseField json = new DatabaseField(COL_JSON, "text");
+    final DatabaseField uuid = new DatabaseField(COL_UUID, "text");
+    final DatabaseField requestId = new DatabaseField(COL_REQUEST_ID, "text");
+    final DatabaseField isNotifiable = new DatabaseField(COL_IS_NOTIFIABLE, "integer");
+    final DatabaseTable tripGroups = new DatabaseTable(
+        TABLE_TRIP_GROUPS,
+        new DatabaseField[] {
+            _id, requestId, uuid, frequency, displayTripId, isNotifiable
+        },
+        UniqueIndices.of(TABLE_TRIP_GROUPS, requestId, uuid),
+        "CREATE TRIGGER deleteTrips AFTER DELETE ON " + TABLE_TRIP_GROUPS + " BEGIN " +
+            "DELETE FROM " + TABLE_TRIPS + " WHERE " + groupId + " = old.uuid;" +
+            "END;"
+    );
+    tripGroups.create(database);
+
+    final DatabaseTable trips = new DatabaseTable(
+        TABLE_TRIPS,
+        new DatabaseField[] {
+            _id, id, groupId, uuid,
+            currencySymbol, saveUrl, depart, arrive,
+            caloriesCost, moneyCost, carbonCost, hassleCost, weightedScore,
+            updateUrl, progressUrl, plannedUrl, tempUrl,
+            queryIsLeaveAfter
+        },
+        UniqueIndices.of(TABLE_TRIPS, id, groupId, uuid),
+        "CREATE TRIGGER deleteSegments AFTER DELETE ON " + TABLE_TRIPS + " BEGIN " +
+            "DELETE FROM " + TABLE_SEGMENTS + " WHERE " + tripId + " = old.uuid;" +
+            "END;"
+    );
+    trips.create(database);
+
+    final DatabaseTable segments = new DatabaseTable(
+        TABLE_SEGMENTS,
+        new DatabaseField[] {_id, tripId, json}
+    );
+    segments.create(database);
+  }
+}
---
+++ route-persistence/src/test/java/com/skedgo/routepersistence/WhereClausesTest.java
@@ -0,0 +1,43 @@
+package com.skedgo.routepersistence;
+
+import android.util.Pair;
+
+import com.skedgo.android.common.model.TripGroup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 22)
+public class WhereClausesTest {
+  @Test public void shouldCreateClauseToMatchUuid() {
+    final TripGroup group = mock(TripGroup.class);
+    when(group.uuid()).thenReturn("Some id");
+
+    final Pair<String, String[]> r = WhereClauses.matchesUuidOf(group);
+
+    assertThat(r.first).isEqualTo("uuid = ?");
+    assertThat(r.second).isEqualTo(new String[] {"Some id"});
+  }
+
+  @Test public void shouldCreateClauseToDetectPastRoutes() {
+    final Pair<String, String[]> r = WhereClauses.happenedBefore(2, TimeUnit.DAYS.toMillis(5L));
+    assertThat(r.first).isEqualTo(
+        "EXISTS (" +
+            "SELECT * FROM trips " +
+            "WHERE tripGroups.uuid = trips.groupId " +
+            "AND tripGroups.displayTripId = trips.id " +
+            "AND trips.arrive < ?" +
+            ")"
+    );
+    assertThat(r.second).isEqualTo(new String[] {"424800"});
+  }
+}
