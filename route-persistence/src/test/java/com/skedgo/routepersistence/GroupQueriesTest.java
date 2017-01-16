afdabb238647452ad9dbc81acc62b7e9ba074303
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-01-16 13:18:11 +0000

    Move `route-persistence` over (#120)
    * Put `route-persistence` code
    * Add android-25 for Travis
    * Upgrade gradlew

---
+++ route-persistence/src/test/java/com/skedgo/routepersistence/GroupQueriesTest.java
@@ -0,0 +1,32 @@
+package com.skedgo.routepersistence;
+
+import android.util.Pair;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 22)
+public class GroupQueriesTest {
+  @Test public void shouldCreateRawQueryToMatchNotifiableRoutes() {
+    final Pair<String, String[]> r = GroupQueries.isNotifiable();
+    assertThat(r.first).isEqualTo("select * from tripGroups where isNotifiable = 1");
+    assertThat(r.second).isNull();
+  }
+
+  @Test public void shouldCreateRawQueryToMatchCorrectRequestId() {
+    final Pair<String, String[]> r = GroupQueries.hasRequestId("Some id");
+    assertThat(r.first).isEqualTo("select * from tripGroups where requestId = ?");
+    assertThat(r.second).containsExactly("Some id");
+  }
+
+  @Test public void shouldCreateRawQueryToMatchCorrectRouteByUuid() {
+    final Pair<String, String[]> r = GroupQueries.hasUuid("Some id");
+    assertThat(r.first).isEqualTo("select * from tripGroups where uuid = ?");
+    assertThat(r.second).containsExactly("Some id");
+  }
+}
---
+++ route-persistence/src/test/java/com/skedgo/routepersistence/LocationTypeAdapterFactoryTest.java
@@ -0,0 +1,41 @@
+package com.skedgo.routepersistence;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.skedgo.android.common.model.Location;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 22)
+public class LocationTypeAdapterFactoryTest {
+  @Test public void shouldOnlyWriteNecessaryProperties() {
+    final Gson gson = new GsonBuilder()
+        .registerTypeAdapterFactory(new LocationTypeAdapterFactory())
+        .create();
+
+    final Location location = new Location(1.0, 2.0);
+    location.setTimeZone("Some timezone");
+    location.setAddress("Some address");
+    location.setName("Some name");
+    location.isFavourite(true);
+    location.setBearing(20);
+    location.setW3wInfoURL("Some w3w info");
+
+    final JsonObject json = gson.toJsonTree(location, Location.class)
+        .getAsJsonObject();
+    assertThat(json.entrySet()).hasSize(6);
+    assertThat(json.has("lat")).isTrue();
+    assertThat(json.has("lng")).isTrue();
+    assertThat(json.has("name")).isTrue();
+    assertThat(json.has("address")).isTrue();
+    assertThat(json.has("timezone")).isTrue();
+    assertThat(json.has("bearing")).isTrue();
+  }
+}
