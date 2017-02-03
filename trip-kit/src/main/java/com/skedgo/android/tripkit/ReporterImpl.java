3f080ad1586227bb9fad6f0b3e9633d6ce0b4313
ntvinh11586 <ntvinh.11586@gmail.com>
2017-02-03 22:56:45 +0700

    Change the rest to new Retrofit syntax

--- trip-kit/src/main/java/com/skedgo/android/tripkit/ReporterImpl.java
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/ReporterImpl.java
@@ -3,14 +3,19 @@ package com.skedgo.android.tripkit;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.google.gson.GsonBuilder;
 import com.skedgo.android.common.model.Trip;
 
 import java.util.Collections;
 import java.util.Map;
 
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import rx.functions.Action1;
 import rx.functions.Actions;
 import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 final class ReporterImpl implements Reporter {
   private final Func1<String, ReportingApi> apiFactory;
@@ -23,11 +28,18 @@ final class ReporterImpl implements Reporter {
     this.errorHandler = errorHandler;
   }
 
+
   @Override public void reportPlannedTrip(
       @NonNull Trip trip,
       @Nullable Map<String, Object> userInfo) {
     if (trip.getPlannedURL() != null) {
-      apiFactory.call(trip.getPlannedURL())
+      new Retrofit.Builder()
+          /* This base url is ignored as the api relies on @Url. */
+          .baseUrl(trip.getPlannedURL() + "/")
+          .addCallAdapterFactory(RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io()))
+          .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().create()))
+          .build()
+          .create(ReportingApi.class)
           .reportPlannedTripAsync(
               userInfo == null
                   ? Collections.<String, Object>emptyMap()
