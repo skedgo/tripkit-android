6f04642941c031e99a4f168131438258eed18597
Thuy Trinh <thuy@skedgo.com>
2017-02-08 18:07:29 +0700

    Clean up

--- validBookingCountCore/src/main/java/com/skedgo/validbookingcount/ValidBookingCountModule.kt
+++ validBookingCountCore/src/main/java/com/skedgo/validbookingcount/ValidBookingCountModule.kt
@@ -5,18 +5,19 @@ import android.content.SharedPreferences
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import rx.schedulers.Schedulers
 import javax.inject.Named
 
-
 @Module
 class ValidBookingCountModule {
-  @Provides fun validBookingCountApi(okHttpClient: okhttp3.OkHttpClient, gson: Gson): ValidBookingCountApi {
+  @Provides
+  fun validBookingCountApi(httpClient: OkHttpClient, gson: Gson): ValidBookingCountApi {
     return Retrofit.Builder()
-        .client(okHttpClient)
+        .client(httpClient)
         .baseUrl("https://tripgo.skedgo.com/satapp/")
         .addCallAdapterFactory(RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io()))
         .addConverterFactory(GsonConverterFactory.create(gson))
@@ -24,7 +25,8 @@ class ValidBookingCountModule {
         .create(ValidBookingCountApi::class.java)
   }
 
-  @Provides @Named("ValidBookingCountPreferences") fun validBookingCountSharedPreferences(context: Context): SharedPreferences {
+  @Provides @Named("ValidBookingCountPreferences")
+  fun validBookingCountPreferences(context: Context): SharedPreferences {
     return context.getSharedPreferences("ValidBookingCountPreferences", Context.MODE_PRIVATE);
   }
 }
--- validBookingCountCore/src/main/java/com/skedgo/validbookingcount/ValidBookingCountRepository.kt
+++ validBookingCountCore/src/main/java/com/skedgo/validbookingcount/ValidBookingCountRepository.kt
@@ -3,41 +3,38 @@ package com.skedgo.validbookingcount
 import android.content.SharedPreferences
 import rx.Emitter
 import rx.Observable
-import rx.schedulers.Schedulers
 import javax.inject.Inject
 import javax.inject.Named
 
 
 private const val KEY_VALID_BOOKING_COUNT = "validBookingCount"
 
-class ValidBookingCountRepository @Inject internal constructor(@Named("ValidBookingCountPreferences") val sharedPreferences: SharedPreferences,
-                                                               private val api: ValidBookingCountApi) {
-
-
+class ValidBookingCountRepository @Inject internal constructor(
+    @Named("ValidBookingCountPreferences") val preferences: SharedPreferences,
+    private val api: ValidBookingCountApi
+) {
   fun validBookingCount(): Observable<Int> {
     return Observable.fromEmitter<Int>({
-      if (sharedPreferences.contains(KEY_VALID_BOOKING_COUNT)) {
-        it.onNext(sharedPreferences.getInt(KEY_VALID_BOOKING_COUNT, -1))
+      if (preferences.contains(KEY_VALID_BOOKING_COUNT)) {
+        it.onNext(preferences.getInt(KEY_VALID_BOOKING_COUNT, -1))
       }
       val listener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
         if (key == KEY_VALID_BOOKING_COUNT) {
           it.onNext(sharedPreferences.getInt(KEY_VALID_BOOKING_COUNT, -1))
         }
       }
-      sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
-      it.setCancellation { sharedPreferences.unregisterOnSharedPreferenceChangeListener(listener) }
+      preferences.registerOnSharedPreferenceChangeListener(listener)
+      it.setCancellation { preferences.unregisterOnSharedPreferenceChangeListener(listener) }
     }, Emitter.BackpressureMode.BUFFER)
-        .subscribeOn(Schedulers.io())
   }
 
   fun updateValidBookingCount(): Observable<Int> {
     return api.fetchValidBookingsCountAsync()
         .map { it.count() }
-        .observeOn(Schedulers.io())
         .doOnNext {
-          sharedPreferences.edit()
+          preferences.edit()
               .putInt(KEY_VALID_BOOKING_COUNT, it)
-              .commit()
+              .apply()
         }
   }
 }
