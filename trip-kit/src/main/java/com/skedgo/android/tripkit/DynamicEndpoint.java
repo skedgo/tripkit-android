1e4fd72392e44c23472dbaca36b13342d5ac24be
Vinh Nguyen <ntvinh11586@users.noreply.github.com>
2017-02-14 18:24:10 +0700

    Migrate retrofit v1.x to retrofit v2.x (#126)
    Fix https://github.com/skedgo/tripkit-android/issues/123

--- trip-kit/src/main/java/com/skedgo/android/tripkit/DynamicEndpoint.java
+++
@@ -1,9 +0,0 @@
-package com.skedgo.android.tripkit;
-
-import android.support.annotation.NonNull;
-
-import retrofit.Endpoint;
-
-interface DynamicEndpoint extends Endpoint {
-  void setUrl(@NonNull String url);
-}
