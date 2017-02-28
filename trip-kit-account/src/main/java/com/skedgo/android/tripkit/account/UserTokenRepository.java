0aecfbd8523c0608747a321218aa297c783a54b8
Thuy Trinh <thuy@skedgo.com>
2017-02-28 20:05:22 +0700

    Open methods to interact w/ UserToken

--- trip-kit-account/src/main/java/com/skedgo/android/tripkit/account/UserTokenRepository.java
+++ trip-kit-account/src/main/java/com/skedgo/android/tripkit/account/UserTokenRepository.java
@@ -2,7 +2,7 @@ package com.skedgo.android.tripkit.account;
 
 import android.support.annotation.Nullable;
 
-public abstract class UserTokenRepository {
-  abstract public String getUserToken();
-  abstract void putUserToken(@Nullable String userToken);
+public interface UserTokenRepository {
+  String getUserToken();
+  void putUserToken(@Nullable String userToken);
 }
