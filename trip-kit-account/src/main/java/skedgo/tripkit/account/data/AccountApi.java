287f972eb777ec98f95b41d3c94bf51dff91e5ae
Thuy Trinh <thuy@skedgo.com>
2017-03-07 01:00:38 +0700

    Hide AccountApi

--- trip-kit-account/src/main/java/skedgo/tripkit/account/data/AccountApi.java
+++ trip-kit-account/src/main/java/skedgo/tripkit/account/data/AccountApi.java
@@ -4,7 +4,7 @@ import retrofit2.http.Body;
 import retrofit2.http.POST;
 import rx.Observable;
 
-public interface AccountApi {
+interface AccountApi {
   @POST("account/signup") Observable<SignUpResponse> signUp(@Body SignUpBody body);
   @POST("account/login") Observable<LogInResponse> logIn(@Body LogInBody body);
   @POST("account/logout") Observable<LogOutResponse> logOut();
