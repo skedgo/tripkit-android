98261cb0375c7c78ba3b3fdfbb224b260391143d
Thuy Trinh <thuy@skedgo.com>
2017-03-07 15:28:36 +0700

    Write tests for SignIn and LogOut

--- AccountDomain/src/main/java/skedgo/tripkit/account/domain/GetUserIdentifier.kt
+++ AccountDomain/src/main/java/skedgo/tripkit/account/domain/GetUserIdentifier.kt
@@ -3,6 +3,10 @@ package skedgo.tripkit.account.domain
 import rx.Observable
 import javax.inject.Inject
 
+/**
+ * Gets an unique identifier representing an user.
+ * The emitted identifier can be used later for [SilentlyLogIn].
+ */
 open class GetUserIdentifier @Inject constructor(
     private val userRepository: UserRepository
 ) {
