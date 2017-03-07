7e3273518b001c2a45b316ff2b0e7e1e400e2bf3
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-03-07 18:04:13 +0700

    Decouple into AccountData and AccountDomain modules (#149)

---
+++ AccountData/src/main/java/skedgo/tripkit/account/data/LogOutResponse.java
@@ -0,0 +1,11 @@
+package skedgo.tripkit.account.data;
+
+import com.google.gson.annotations.JsonAdapter;
+
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
+
+@Gson.TypeAdapters
+@Value.Immutable
+@JsonAdapter(GsonAdaptersLogOutResponse.class)
+abstract class LogOutResponse {}
