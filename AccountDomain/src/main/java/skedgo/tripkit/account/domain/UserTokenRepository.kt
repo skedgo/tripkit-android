09fae74a2db319fa18b32c67de49fdbbf4abc051
Thuy Trinh <thuy@skedgo.com>
2017-03-07 14:51:07 +0700

    Write a first test for GetUserIdentifier

--- AccountDomain/src/main/java/skedgo/tripkit/account/domain/UserTokenRepository.kt
+++ AccountDomain/src/main/java/skedgo/tripkit/account/domain/UserTokenRepository.kt
@@ -3,7 +3,11 @@ package skedgo.tripkit.account.domain
 import rx.Observable
 
 interface UserTokenRepository {
+  /**
+   * If there is no user token, this returns an empty [Observable].
+   */
   fun getLastKnownUserToken(): Observable<UserToken>
+
   fun getUserTokenByUserIdentifier(userIdentifier: String): Observable<UserToken>
   fun getUserTokenBySignUpCredentials(signUpCredentials: SignUpCredentials): Observable<UserToken>
   fun getUserTokenBySignInCredentials(signInCredentials: SignInCredentials): Observable<UserToken>
