749a97eb03cfce6376f2d00ec3437f456ae045cb
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-03-10 20:13:20 +0700

    Check UserToken before fetching valid booking count (#154)

---
+++ ValidBookingCountDomain/src/main/java/skedgo/tripkit/validbookingcount/domain/GetRemoteValidBookingCount.kt
@@ -0,0 +1,19 @@
+package skedgo.tripkit.validbookingcount.domain
+
+import rx.Observable
+import skedgo.tripkit.account.domain.HasUserToken
+import javax.inject.Inject
+
+/**
+ * Gotta check user token existence. Otherwise, it results in
+ * errors of missing user token at server side.
+ */
+open class GetRemoteValidBookingCount @Inject constructor(
+    private val hasUserToken: HasUserToken,
+    private val validBookingCountRepository: ValidBookingCountRepository
+) {
+  open fun execute(): Observable<Int>
+      = hasUserToken.execute()
+      .filter { it }
+      .flatMap { validBookingCountRepository.getRemoteValidBookingCount() }
+}
