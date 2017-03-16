fbdbb41bafae45e8057394319ba92ed95427bc50
Emmanuel Lagarrigue <emmanuel.lagarrigue@black-tobacco.com>
2017-03-16 11:00:46 -0300

    Remove unused dependencies

--- trip-kit-booking-ui/src/main/java/com/skedgo/android/tripkit/booking/ui/BookingUiModule.java
+++ trip-kit-booking-ui/src/main/java/com/skedgo/android/tripkit/booking/ui/BookingUiModule.java
@@ -2,16 +2,13 @@ package com.skedgo.android.tripkit.booking.ui;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.util.Log;
 
 import com.google.gson.Gson;
 import com.jakewharton.picasso.OkHttp3Downloader;
 import com.skedgo.android.common.util.Gsons;
-import com.skedgo.android.common.util.MainThreadBus;
 import com.skedgo.android.tripkit.TripKit;
 import com.skedgo.android.tripkit.booking.BookingService;
 import com.skedgo.android.tripkit.booking.ExternalOAuthService;
-import com.squareup.otto.Bus;
 import com.squareup.picasso.Picasso;
 
 import javax.inject.Singleton;
@@ -19,7 +16,6 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
-import rx.functions.Action1;
 
 @Module
 public class BookingUiModule {
@@ -43,16 +39,6 @@ public class BookingUiModule {
 
   @Provides Gson gson() {
     return Gsons.createForLowercaseEnum();
-  }
-
-  @Provides @Singleton Bus bus() {
-    return new MainThreadBus(new Action1<Throwable>() {
-      @Override public void call(Throwable error) {
-        if (BuildConfig.DEBUG) {
-          Log.e(MainThreadBus.class.getSimpleName(), error.getMessage(), error);
-        }
-      }
-    });
   }
 
   @Provides @Singleton
