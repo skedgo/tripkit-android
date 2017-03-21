e2186cb192c63918517b019e8322189ae69c622a
Thuy Trinh <thuy@skedgo.com>
2017-03-21 10:59:36 +0700

    Create TripKit

---
+++ A2bTrips/src/main/java/skedgo/tripkit/demo/a2btrips/App.kt
@@ -0,0 +1,19 @@
+package skedgo.tripkit.demo.a2btrips
+
+import android.app.Application
+import com.skedgo.android.tripkit.Configs
+import com.skedgo.android.tripkit.TripKit
+import net.danlew.android.joda.JodaTimeAndroid
+
+class App : Application() {
+  override fun onCreate() {
+    super.onCreate()
+    JodaTimeAndroid.init(this)
+    TripKit.initialize(
+        Configs.builder()
+            .context(this)
+            .regionEligibility("beta")
+            .build()
+    )
+  }
+}
