00b960ec4d5a55c84c609f4856c968f27086e2b4
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-03-22 18:53:47 +0700

    Move IsEventIncludedDomain and IsEventIncludedData out (#160)

--- IsEventIncludedDomain/src/main/java/skedgo/iseventincluded/domain/IsEventIncludedRepository.kt
+++
@@ -1,8 +0,0 @@
-package skedgo.iseventincluded.domain
-
-import rx.Observable
-
-interface IsEventIncludedRepository {
-  fun setIsEventIncluded(eventId: String, isEventIncluded: IsEventIncluded): Observable<Unit>
-  fun getIsEventIncluded(eventId: String): Observable<IsEventIncluded>
-}
