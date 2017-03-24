d1237f10dd9ee6074ca2cf6af253fa672218210b
Tin Tran <thuutin@gmail.com>
2017-03-24 16:36:41 +0700

    Make use of collection-binding-adatper 2

---
+++ A2bTrips/src/main/java/skedgo/tripkit/demo/a2btrips/TripDetailsViewModel.kt
@@ -0,0 +1,23 @@
+package skedgo.tripkit.demo.a2btrips
+
+import android.content.Context
+import android.databinding.ObservableArrayList
+import android.databinding.ObservableList
+import com.skedgo.android.common.model.Trip
+import me.tatarka.bindingcollectionadapter2.ItemBinding
+import rx.Observable
+
+
+class TripDetailsViewModel constructor(context: Context, trip: Trip) {
+  val segmentBinding = ItemBinding.of<TripSegmentViewModel>(BR.viewModel, R.layout.segment)
+  val tripSegments: ObservableList<TripSegmentViewModel> = ObservableArrayList()
+
+  init {
+    Observable.from(trip.segments)
+        .map { TripSegmentViewModel(context, it) }
+        .toList()
+        .subscribe {
+          tripSegments.addAll(it)
+        }
+  }
+}
---
+++ A2bTrips/src/main/java/skedgo/tripkit/demo/a2btrips/TripSegmentViewModel.kt
@@ -0,0 +1,26 @@
+package skedgo.tripkit.demo.a2btrips
+
+import android.content.Context
+import android.databinding.ObservableField
+import android.databinding.ObservableInt
+import android.text.TextUtils
+import android.view.View
+import com.skedgo.android.common.model.TripSegment
+import com.skedgo.android.common.util.TripSegmentUtils
+
+
+class TripSegmentViewModel constructor(context: Context, tripSegment: TripSegment) {
+
+  val actionTitle by lazy {
+    ObservableField<String>(TripSegmentUtils.getTripSegmentAction(context, tripSegment))
+  }
+
+  val hasActionNotes by lazy {
+    ObservableInt(if (!TextUtils.isEmpty(tripSegment.notes)) View.VISIBLE else View.GONE)
+  }
+
+  val actionNotes by lazy {
+    ObservableField<String>(tripSegment.getDisplayNotes(context.resources))
+  }
+
+}
