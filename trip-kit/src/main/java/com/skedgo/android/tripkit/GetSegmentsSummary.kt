6ede7ff992c860e77fcfdb8a68a61d77c0d313ef
Tin Tran <thuutin@gmail.com>
2017-03-27 17:04:53 +0700

    Move getSummarySegments to the UseCase

--- trip-kit/src/main/java/com/skedgo/android/tripkit/GetSegmentsSummary.kt
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/GetSegmentsSummary.kt
@@ -1,12 +1,19 @@
 package com.skedgo.android.tripkit
 
+import com.skedgo.android.common.model.SegmentType
 import com.skedgo.android.common.model.Trip
 import com.skedgo.android.common.model.TripSegment
 import rx.Observable
+import javax.inject.Inject
 
 
-class GetSegmentsSummary {
+class GetSegmentsSummary @Inject constructor() {
   fun execute(trip: Trip): Observable<List<TripSegment>> {
-    return Observable.fromCallable { trip.summarySegments }
+    return Observable.fromCallable <List<TripSegment>> { trip.segments }
+        .filter { it != null }
+        .defaultIfEmpty(emptyList<TripSegment>())
+        .flatMap { Observable.from(it) }
+        .filter { it.type != SegmentType.ARRIVAL && it.isVisibleInContext(TripSegment.VISIBILITY_IN_SUMMARY) }
+        .toList()
   }
 }
