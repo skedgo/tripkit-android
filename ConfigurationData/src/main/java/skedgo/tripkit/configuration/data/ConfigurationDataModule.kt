9da5e9d11dd6489a3829fc200f8b999c41b23ccc
Thuy Trinh <thuy@skedgo.com>
2017-03-28 16:01:24 +0700

    Remove old region eligibility code

--- ConfigurationData/src/main/java/skedgo/tripkit/configuration/data/ConfigurationDataModule.kt
+++ ConfigurationData/src/main/java/skedgo/tripkit/configuration/data/ConfigurationDataModule.kt
@@ -4,16 +4,9 @@ import android.content.Context
 import dagger.Module
 import dagger.Provides
 import skedgo.tripkit.configuration.domain.ApiKeyRepository
-import skedgo.tripkit.configuration.domain.RegionEligibilityRepository
 
 @Module
 class ConfigurationDataModule {
-  @Provides fun regionEligibilityRepository(context: Context): RegionEligibilityRepository
-      = RegionEligibilityRepositoryImpl(context.getSharedPreferences(
-      "RegionEligibilityPreferences",
-      Context.MODE_PRIVATE
-  ))
-
   @Provides fun apiKeyRepository(context: Context): ApiKeyRepository
       = ApiKeyRepositoryImpl(context.getSharedPreferences(
       "ApiKeyPrefs",
