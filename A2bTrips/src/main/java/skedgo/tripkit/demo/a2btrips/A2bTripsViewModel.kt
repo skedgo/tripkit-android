971ad5bf18954b383d034b34fb368fb5c6082607
Thuy Trinh <thuy@skedgo.com>
2017-03-30 16:57:33 +0700

    Use the new A2bRoutingRequest

--- A2bTrips/src/main/java/skedgo/tripkit/demo/a2btrips/A2bTripsViewModel.kt
+++ A2bTrips/src/main/java/skedgo/tripkit/demo/a2btrips/A2bTripsViewModel.kt
@@ -3,13 +3,17 @@ package skedgo.tripkit.demo.a2btrips
 import android.content.Context
 import android.databinding.ObservableArrayList
 import android.databinding.ObservableList
-import com.skedgo.android.common.model.*
+import com.skedgo.android.common.model.Trip
+import com.skedgo.android.common.model.TripGroupComparators
 import com.skedgo.android.tripkit.TripKit
 import me.tatarka.bindingcollectionadapter2.ItemBinding
+import org.joda.time.DateTime
 import rx.Observable
 import rx.android.schedulers.AndroidSchedulers.mainThread
 import rx.subjects.BehaviorSubject
 import rx.subjects.PublishSubject
+import skedgo.tripkit.routing.a2b.A2bRoutingRequest
+import skedgo.tripkit.routing.a2b.RequestTime.DepartNow
 
 class A2bTripsViewModel constructor(
     private val context: Context
@@ -22,22 +26,19 @@ class A2bTripsViewModel constructor(
 
   fun showSampleTrips(): Observable<Unit>
       = Observable
-      .fromCallable {
-        val origin = Location(34.193984, -118.392930)
-        origin.address = "11923 Vanowen St, North Hollywood, CA 91605, USA"
-
-        val destination = Location(34.184923, -118.353576)
-        destination.address = "2001-2027 N Maple St, Burbank, CA 91505, USA"
-
-        val query = Query()
-        query.fromLocation = origin
-        query.toLocation = destination
-        query.setTimeTag(TimeTag.createForLeaveNow())
-        query
+      .defer {
+        TripKit.singleton().getA2bTrips.execute(
+            A2bRoutingRequest.builder()
+                .origin(Pair(34.193984, -118.392930))
+                .originAddress("11923 Vanowen St, North Hollywood, CA 91605, USA")
+                .destination(Pair(34.184923, -118.353576))
+                .destinationAddress("2001-2027 N Maple St, Burbank, CA 91505, USA")
+                .time(DepartNow { DateTime.now() })
+                .build()
+        )
       }
-      .flatMap { TripKit.singleton().routeService.routeAsync(it) }
       .flatMap { Observable.from(it) }
-      .toSortedList { tripGroupA, tripGroupB -> TripGroupComparators.ARRIVAL_COMPARATOR_CHAIN.compare(tripGroupA, tripGroupB) }
+      .toSortedList { lhs, rhs -> TripGroupComparators.ARRIVAL_COMPARATOR_CHAIN.compare(lhs, rhs) }
       .doOnSubscribe { _isRefreshing.onNext(true) }
       .doOnUnsubscribe { _isRefreshing.onNext(false) }
       .observeOn(mainThread())
