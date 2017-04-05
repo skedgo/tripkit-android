e71756ddded8fbe5ba7d38202753233cc72d48a6
Thuy Trinh <thuy@skedgo.com>
2017-04-06 00:17:06 +0700

    Remove empty lines and align enum values

--- trip-kit/src/main/java/skedgo/tripkit/analytics/TripSource.kt
+++ trip-kit/src/main/java/skedgo/tripkit/analytics/TripSource.kt
@@ -1,7 +1,10 @@
 package skedgo.tripkit.analytics
 
-
 enum class TripSource(val type: String) {
-  Agenda("agenda"), Booking("booking"), External("external"), Favorite("favorite"), Manual("manual"), Unknown("unknown")
+  Agenda("agenda"),
+  Booking("booking"),
+  External("external"),
+  Favorite("favorite"),
+  Manual("manual"),
+  Unknown("unknown")
 }
-
