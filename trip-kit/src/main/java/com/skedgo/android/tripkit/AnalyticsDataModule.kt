ca0e2ebf7f2f38d1851fbce042b1e6162fae1a13
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-04-06 17:07:28 +0700

    Move TripGo-related app logic out of TripKit (#178)

--- trip-kit/src/main/java/com/skedgo/android/tripkit/AnalyticsDataModule.kt
+++ trip-kit/src/main/java/com/skedgo/android/tripkit/AnalyticsDataModule.kt
@@ -8,6 +8,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import rx.schedulers.Schedulers
+import skedgo.tripkit.analytics.MarkTripAsPlannedWithUserInfo
 
 @Module
 class AnalyticsDataModule {
--- trip-kit/src/main/java/skedgo/tripkit/analytics/UserInfo.kt
+++
@@ -1,3 +0,0 @@
-package skedgo.tripkit.analytics
-
-data class UserInfo(val source: TripSource, val choiceSet: List<Choice>)
