175159db8e9b2bd5317b669f924ad712c82b6b9a
Thuy Trinh <thuy@skedgo.com>
2017-04-06 12:52:35 +0700

    Encapsulate toMap()

--- trip-kit/src/main/java/skedgo/tripkit/analytics/UserInfoMapMapper.kt
+++ trip-kit/src/main/java/skedgo/tripkit/analytics/UserInfoMapMapper.kt
@@ -1,6 +1,6 @@
 package skedgo.tripkit.analytics
 
-fun UserInfo.toMap(): Map<String, Any> =
+internal fun UserInfo.toMap(): Map<String, Any> =
     mapOf<String, Any>(
         "source" to this.source as Any,
         "choiceSet" to this.choiceSet as Any
