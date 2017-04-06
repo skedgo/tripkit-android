9aab06cd75eb299d1ea4b65ccadbc28c48949c0a
Emmanuel Lagarrigue <emmanuel.lagarrigue@black-tobacco.com>
2017-04-05 21:07:51 -0300

    Add base url parameter for booking server

--- trip-kit-booking/src/main/java/com/skedgo/android/tripkit/booking/mybookings/MyBookingsApi.java
+++ trip-kit-booking/src/main/java/com/skedgo/android/tripkit/booking/mybookings/MyBookingsApi.java
@@ -2,13 +2,14 @@ package com.skedgo.android.tripkit.booking.mybookings;
 
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import retrofit2.http.Url;
 import rx.Observable;
 
 public interface MyBookingsApi {
-  @GET("booking")
-  Observable<MyBookingsResponse> fetchMyBookingsAsync(
-      @Query("first") int first,
-      @Query("max") int pageSize,
-      @Query("bsb") int bsb
+  @GET()
+  Observable<MyBookingsResponse> fetchMyBookingsAsync(@Url String url,
+                                                      @Query("first") int first,
+                                                      @Query("max") int pageSize,
+                                                      @Query("bsb") int bsb
   );
 }
