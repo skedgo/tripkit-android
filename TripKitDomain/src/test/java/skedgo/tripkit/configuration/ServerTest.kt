14838e25df95e307c2cace81364ed5171a91b074
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-04-29 15:34:28 +0700

    Import members

--- TripKitDomain/src/test/java/skedgo/tripkit/configuration/ServerTest.kt
+++ TripKitDomain/src/test/java/skedgo/tripkit/configuration/ServerTest.kt
@@ -1,11 +1,11 @@
 package skedgo.tripkit.configuration
 
-import org.assertj.core.api.Java6Assertions
+import org.assertj.core.api.Java6Assertions.assertThat
 import org.junit.Test
 
 class ServerTest {
   @Test fun `Server should end with a slash`() {
-    Java6Assertions.assertThat(Server.Inflationary.value).endsWith("/")
-    Java6Assertions.assertThat(Server.BigBang.value).endsWith("/")
+    assertThat(Server.Inflationary.value).endsWith("/")
+    assertThat(Server.BigBang.value).endsWith("/")
   }
 }
