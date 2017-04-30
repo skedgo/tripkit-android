1b1cb3194960874f58f604ef2507002cc99043d7
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-05-01 00:21:50 +0700

    Write PrintFullDateImplTest

--- TripKitData/src/main/java/skedgo/tripkit/datetime/PrintFullDateImpl.kt
+++ TripKitData/src/main/java/skedgo/tripkit/datetime/PrintFullDateImpl.kt
@@ -2,30 +2,33 @@ package skedgo.tripkit.datetime
 
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
-import org.joda.time.format.DateTimeFormatter
 import rx.Observable
 import rx.schedulers.Schedulers
 import java.text.DateFormat
 import java.util.*
 
 internal class PrintFullDateImpl constructor(
-    private val getJodaFullDateFormatter: () -> DateTimeFormatter = {
-      DateTimeFormat.fullDate()
+    private val getLocale: () -> Locale = { Locale.getDefault() },
+    private val printFullDateByJoda: (DateTime, Locale) -> String = {
+      dateTime, locale ->
+      DateTimeFormat.fullDate().withLocale(locale).print(dateTime)
     },
-    private val getJavaFullDateFormat: () -> DateFormat = {
-      DateFormat.getDateInstance(DateFormat.FULL, Locale.getDefault())
+    private val printFullDateByJava: (Date, Locale) -> String = {
+      date, locale ->
+      DateFormat.getDateInstance(DateFormat.FULL, locale).format(date)
     }
 ) : PrintFullDate {
   override fun execute(dateTime: DateTime): Observable<String>
       = Observable
       .fromCallable {
-        getJodaFullDateFormatter().print(dateTime)
+        // Still relies on Joda because of its up-to-date timezone database.
+        printFullDateByJoda(dateTime, getLocale())
       }
       .onErrorResumeNext {
         // To handle https://fabric.io/skedgo/android/apps/com.buzzhives.android.tripplanner/issues/588311cd0aeb16625b5aeda1.
         when (it) {
           is IllegalArgumentException -> Observable.fromCallable {
-            getJavaFullDateFormat().format(dateTime.toDate())
+            printFullDateByJava(dateTime.toDate(), getLocale())
           }
           else -> Observable.error(it)
         }
