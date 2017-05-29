868f44bf419c7c0911f1d554d1bf15a5b5be5683
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-05-29 22:45:46 +0700

    Upgrade Kotlin & Dagger

--- TripKitAndroid/src/main/java/skedgo/tripkit/android/A2bRoutingComponent.kt
+++ TripKitAndroid/src/main/java/skedgo/tripkit/android/A2bRoutingComponent.kt
@@ -3,12 +3,11 @@ package skedgo.tripkit.android
 import dagger.Subcomponent
 import skedgo.tripkit.a2brouting.GetA2bRoutingResults
 import skedgo.tripkit.a2brouting.GetTripLine
-import javax.inject.Singleton
 
 /**
  * Creates UseCases and Repositories related to the A2bRouting feature.
  */
-@Singleton
+@FeatureScope
 @Subcomponent
 interface A2bRoutingComponent {
   val getA2bRoutingResults: GetA2bRoutingResults
