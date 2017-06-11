26979c1f8860e9a4102903de4911cc7986d17772
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-06-11 22:24:10 +0700

    Welcome to TripKit, GeoPoint

---
+++ TripKitDomain/src/main/java/skedgo/tripkit/location/GeoPoint.kt
@@ -0,0 +1,10 @@
+package skedgo.tripkit.location
+
+/**
+ * Represents a point on the Earth's surface, in latitude and longitude coordinates.
+ * Nothing more!
+ */
+data class GeoPoint(
+    val latitude: Double,
+    val longitude: Double
+)
