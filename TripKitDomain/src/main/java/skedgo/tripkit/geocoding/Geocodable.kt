416e5c57efcc1a7420278ed23848fe7763b3fe89
TinTran <thuutin@gmail.com>
2017-12-14 15:53:50 +0700

    Add Geocodable

--- TripKitDomain/src/main/java/skedgo/tripkit/geocoding/Geocodable.kt
+++ TripKitDomain/src/main/java/skedgo/tripkit/geocoding/Geocodable.kt
@@ -5,3 +5,7 @@ import rx.Observable
 interface Geocodable {
   fun getAddress(latitude: Double, longitude: Double): Observable<String>
 }
+
+interface ReverseGeoCodable {
+  fun getCoordinates(address: String): Observable<Pair<Double, Double>>
+}
