780cae2fda14b9b0cad3a88206b71bd0340de5f6
TinTran <thuutin@gmail.com>
2018-01-29 16:09:10 +0700

    Delete table

--- route-persistence/src/main/java/com/skedgo/routepersistence/ManualTripsContract.kt
+++ route-persistence/src/main/java/com/skedgo/routepersistence/ManualTripsContract.kt
@@ -8,24 +8,16 @@ import skedgo.sqlite.UniqueIndices
 
 object ManualTripsContract {
 
-  const val TRIPGROUP_ID = "requestId"
-  const val DURATION_STAYING_AT_START_IN_SECONDS = "durationAtStart"
+  const val TRIPGROUP_ID = "tripGroupId"
+  const val MANUAL_TRIP_ID = "manualTripId"
   const val DURATION_STAYING_AT_END_IN_SECONDS = "durationAtEnd"
   const val MANUAL_TRIPS = "ManualTripsContract"
 
   fun create(database: SQLiteDatabase) {
-    val tripGroupId = DatabaseField(TRIPGROUP_ID, "TEXT", "PRIMARY KEY")
-//    val durationAtStart = DatabaseField(DURATION_STAYING_AT_START_IN_SECONDS, "INTEGER")
-//    val durationAtEnd = DatabaseField(DURATION_STAYING_AT_END_IN_SECONDS, "INTEGER")
-//    DatabaseTable(
-//        MANUAL_TRIPS,
-//        arrayOf(tripGroupId, durationAtStart, durationAtEnd),
-//        "FOREIGN KEY($TRIPGROUP_ID) REFERENCES $TABLE_TRIP_GROUPS($COL_UUID)",
-//        UniqueIndices.of(MANUAL_TRIPS, tripGroupId)
-//    ).create(database)
+    val tripGroupId = DatabaseField(TRIPGROUP_ID, "TEXT", "NON NULL")
     database.execSQL("CREATE TABLE $MANUAL_TRIPS(" +
-        "$TRIPGROUP_ID TEXT PRIMARY KEY," +
-        "$DURATION_STAYING_AT_START_IN_SECONDS INTEGER," +
+        "$TRIPGROUP_ID TEXT NON NULL," +
+        "$MANUAL_TRIP_ID TEXT NON NULL PRIMARY KEY," +
         "$DURATION_STAYING_AT_END_IN_SECONDS INTEGER," +
         "FOREIGN KEY($TRIPGROUP_ID) REFERENCES $TABLE_TRIP_GROUPS($COL_UUID))")
     database.execSQL(UniqueIndices.of(MANUAL_TRIPS, tripGroupId))
