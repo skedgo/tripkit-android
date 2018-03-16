d9723e4f71380bb40c41fbf61b7a68d92d064110
Emmanuel Lagarrigue <emmanuel.lagarrigue@black-tobacco.com>
2018-03-16 14:48:22 -0300

    Add trip segments plain text

--- CommonCoreLegacy/src/main/java/skedgo/tripkit/routing/LocationExtensions.kt
+++ CommonCoreLegacy/src/main/java/skedgo/tripkit/routing/LocationExtensions.kt
@@ -2,7 +2,18 @@ package skedgo.tripkit.routing
 
 import com.skedgo.android.common.model.Location
 import org.joda.time.DateTimeZone
+import java.lang.Math.abs
+
+const val SimilarLocationDegreeDifference = 0.000025
 
 val Location.dateTimeZone get(): DateTimeZone = timeZone
     ?.let { DateTimeZone.forID(it) }
     ?: DateTimeZone.getDefault()
+
+// https://github.com/skedgo/tripkit-ios/blob/master/TripKit/Classes/core/RootKit/Model/SGLocationHelper.m#L240
+fun Location.isNear(location: Location) : Boolean {
+  val latDiff = lat - location.lat
+  val lngDiff = lon - location.lon
+  val maxDiff = SimilarLocationDegreeDifference * 2
+  return abs(latDiff) < maxDiff && abs(lngDiff) < maxDiff
+}
