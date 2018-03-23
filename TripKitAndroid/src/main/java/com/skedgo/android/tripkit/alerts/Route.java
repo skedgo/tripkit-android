0d71c0f3b35c067d4cc0c0bcc6f072210c642830
Tran Huu Tin <thuutin@gmail.com>
2018-03-23 18:10:29 +0700

    Implement service alerts (#274)
    * Deprecate serviceTripID and stopCode
    
    * WIP displaying list of alerts
    
    * Add some @nullable s
    
    * Add Route model
    
    * Add number field
    
    * Update models
    
    * Add kotlin-kapt
    
    * Add lastUpdated
    
    * Add @Nullable for backward compat
    
    * Use Default instead
    
    * Nullable some beta only attr
    
    * Allow Nullable modeInfo

---
+++ TripKitAndroid/src/main/java/com/skedgo/android/tripkit/alerts/Route.java
@@ -0,0 +1,27 @@
+package com.skedgo.android.tripkit.alerts;
+
+import android.support.annotation.Nullable;
+
+import com.google.gson.annotations.JsonAdapter;
+
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
+
+import skedgo.tripkit.routing.ModeInfo;
+
+@Value.Immutable
+@Gson.TypeAdapters
+@JsonAdapter(GsonAdaptersRoute.class)
+public abstract class Route {
+  public abstract String id();
+
+  public abstract String name();
+
+  @Nullable public abstract String number();
+
+  @Nullable public abstract ModeInfo modeInfo();
+
+  @Value.Default int type() {
+    return -1;
+  }
+}
