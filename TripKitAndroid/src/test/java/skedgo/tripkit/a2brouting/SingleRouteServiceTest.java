9d4b32ec9cd055de7add2d8b95c5f4be84735950
Tin Tran <thuutin@gmail.com>
2018-04-06 23:45:21 +0700

    Fix tests

--- TripKitAndroid/src/test/java/skedgo/tripkit/a2brouting/SingleRouteServiceTest.java
+++ TripKitAndroid/src/test/java/skedgo/tripkit/a2brouting/SingleRouteServiceTest.java
@@ -1,6 +1,8 @@
 package skedgo.tripkit.a2brouting;
 
 import com.skedgo.android.common.model.Query;
+import com.skedgo.android.tripkit.DefaultModeFilter;
+import com.skedgo.android.tripkit.ModeFilter;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -22,6 +24,8 @@ import static org.mockito.Mockito.when;
 public class SingleRouteServiceTest {
   @Rule public MockitoRule rule = MockitoJUnit.rule();
   @Mock RouteService routeService;
+
+  ModeFilter modeFilter = new DefaultModeFilter();
   private SingleRouteService singleRouteService;
 
   @Before public void before() {
@@ -36,14 +40,14 @@ public class SingleRouteServiceTest {
   @Test public void shouldCancelPreviousRequest_withQuery() {
     final PublishSubject<List<TripGroup>> emitter1 = PublishSubject.create();
     final PublishSubject<List<TripGroup>> emitter2 = PublishSubject.create();
-    when(routeService.routeAsync(any(Query.class)))
+    when(routeService.routeAsync(any(Query.class), modeFilter))
         .thenReturn(emitter1.asObservable())
         .thenReturn(emitter2.asObservable());
 
-    singleRouteService.routeAsync(mock(Query.class)).subscribe();
+    singleRouteService.routeAsync(mock(Query.class), modeFilter).subscribe();
     assertThat(emitter1.hasObservers()).isTrue();
 
-    singleRouteService.routeAsync(mock(Query.class)).subscribe();
+    singleRouteService.routeAsync(mock(Query.class), modeFilter).subscribe();
     assertThat(emitter1.hasObservers()).isFalse();
     assertThat(emitter2.hasObservers()).isTrue();
   }
