5cb46155db9a749f2189405902a406299f27689e
Tin Tran <thuutin@gmail.com>
2018-06-20 11:24:12 +0700

    Keep this class

--- CommonCoreLegacy/src/main/java/com/skedgo/android/common/model/TransportMode.java
+++ CommonCoreLegacy/src/main/java/com/skedgo/android/common/model/TransportMode.java
@@ -1,6 +1,7 @@
 package com.skedgo.android.common.model;
 
 import android.support.annotation.DrawableRes;
+import android.support.annotation.Keep;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 
 import skedgo.tripkit.routing.ServiceColor;
 
+@Keep
 public class TransportMode {
   public static final String MIDDLE_FIX_CAR = "car-s";
   public static final String MIDDLE_FIX_BIC = "bic-s";
