b45019e2cea63496e3dae718482845e0db374b44
Emmanuel Lagarrigue Lazarte <emmanuel.lagarrigue@black-tobacco.com>
2018-07-23 07:44:11 -0300

    UUID as user identifier (#298)
    * Generate random UUID to use as user identifier
    
    * Fix test
    
    * Move set key responsibility to repository implementation
    
    * Refactor
    
    * Use the same Prefs as the userToken
    
    * Fix test
    
    * Bundle randome uuid
    
    * Remove import
    
    * Fix import

--- AccountDomain/src/main/java/skedgo/tripkit/account/domain/User.kt
+++
@@ -1,3 +0,0 @@
-package skedgo.tripkit.account.domain
-
-data class User(val name: String, val type: String)
--- AccountDomain/src/main/java/skedgo/tripkit/account/domain/UserRepository.kt
+++
@@ -1,7 +0,0 @@
-package skedgo.tripkit.account.domain
-
-import rx.Observable
-
-interface UserRepository {
-  fun getUser(): Observable<User>
-}
