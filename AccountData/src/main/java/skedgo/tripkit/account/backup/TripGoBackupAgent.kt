2875a3242634a82fb5cc17e13ca51c401f7fc989
Emmanuel Lagarrigue <emmanuel.lagarrigue@black-tobacco.com>
2018-07-25 09:30:50 -0300

    Fix format

--- AccountData/src/main/java/skedgo/tripkit/account/backup/TripGoBackupAgent.kt
+++ AccountData/src/main/java/skedgo/tripkit/account/backup/TripGoBackupAgent.kt
@@ -8,8 +8,8 @@ val PREFS_BACKUP_KEY = "account_prefs"
 
 internal class TripGoBackupAgent : BackupAgentHelper() {
 
-    override fun onCreate() {
-        val helper = SharedPreferencesBackupHelper(this, AccountDataModule.UserTokenPreferences)
-        addHelper(PREFS_BACKUP_KEY, helper)
-    }
+  override fun onCreate() {
+    val helper = SharedPreferencesBackupHelper(this, AccountDataModule.UserTokenPreferences)
+    addHelper(PREFS_BACKUP_KEY, helper)
+  }
 }
--- AccountData/src/main/java/skedgo/tripkit/account/data/UserKeyRepositoryImpl.kt
+++ AccountData/src/main/java/skedgo/tripkit/account/data/UserKeyRepositoryImpl.kt
@@ -9,32 +9,32 @@ import java.util.*
 
 
 internal class UserKeyRepositoryImpl(
-        private val prefs: SharedPreferences,
-        private val backupManager: BackupManager
+    private val prefs: SharedPreferences,
+    private val backupManager: BackupManager
 ) : UserKeyRepository {
 
-    private val KEY_USER_UUID = "userUUID"
+  private val KEY_USER_UUID = "userUUID"
 
-    override fun getUserKey(): Single<String> =
-            Single.fromCallable {
-                prefs.getString(KEY_USER_UUID, null)
-            }.flatMap { key ->
-                when {
-                    key.isNullOrEmpty() ->
-                        UUID.randomUUID().toString().let {
-                            setUserKey(it).andThen(Single.just(it))
-                        }
-                    else -> Single.just(key)
-                }
+  override fun getUserKey(): Single<String> =
+      Single.fromCallable {
+        prefs.getString(KEY_USER_UUID, null)
+      }.flatMap { key ->
+        when {
+          key.isNullOrEmpty() ->
+            UUID.randomUUID().toString().let {
+              setUserKey(it).andThen(Single.just(it))
             }
+          else -> Single.just(key)
+        }
+      }
 
-    private fun setUserKey(userKey: String): Completable =
-            Completable.fromCallable {
-                prefs.edit()
-                        .putString(KEY_USER_UUID, userKey)
-                        .apply()
-            }
-                    .andThen(Completable.fromCallable {
-                        backupManager.dataChanged()
-                    })
+  private fun setUserKey(userKey: String): Completable =
+      Completable.fromCallable {
+        prefs.edit()
+            .putString(KEY_USER_UUID, userKey)
+            .apply()
+      }
+          .andThen(Completable.fromCallable {
+            backupManager.dataChanged()
+          })
 }
