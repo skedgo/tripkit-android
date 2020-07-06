7f8281426fdde19dba8784fbbef765263be02825
Matt Barringer <matt@incoherent.de>
2020-07-06 09:42:45 +0200

    Add some fields to the Service response

--- TripKitAndroid/src/main/java/com/skedgo/tripkit/ServiceApi.java
+++ TripKitAndroid/src/main/java/com/skedgo/tripkit/ServiceApi.java
@@ -8,6 +8,9 @@ public interface ServiceApi {
   @GET("service.json") Observable<ServiceResponse> getServiceAsync(
       @Query("region") String region,
       @Query("serviceTripID") String serviceTripId,
+      @Query("operator") String operator,
+      @Query("startStopCode") String startStopCode,
+      @Query("endStopCode") String endStopCode,
       @Query("embarkationDate") long timeInSecs,
       @Query("encode") boolean encode
   );
--- TripKitAndroid/src/main/java/com/skedgo/tripkit/ServiceResponse.java
+++ TripKitAndroid/src/main/java/com/skedgo/tripkit/ServiceResponse.java
@@ -3,6 +3,9 @@ package com.skedgo.tripkit;
 import androidx.annotation.Nullable;
 
 import com.google.gson.annotations.JsonAdapter;
+import com.skedgo.tripkit.alerts.AlertBlock;
+import com.skedgo.tripkit.alerts.ModeInfo;
+import com.skedgo.tripkit.common.model.RealtimeAlert;
 import com.skedgo.tripkit.routing.RealTimeVehicle;
 import com.skedgo.tripkit.routing.Shape;
 
@@ -17,9 +20,10 @@ import static org.immutables.value.Value.Style;
 @Style(passAnnotations = JsonAdapter.class)
 @JsonAdapter(GsonAdaptersServiceResponse.class)
 public interface ServiceResponse {
-  String realTimeStatus();
-  String type();
+  @Nullable String realTimeStatus();
   List<Shape> shapes();
+  @Nullable ModeInfo modeInfo();
   @Nullable RealTimeVehicle realtimeVehicle();
   @Nullable List<RealTimeVehicle> realtimeAlternativeVehicle();
+  @Nullable List<RealtimeAlert> alerts();
 }
