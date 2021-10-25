cac6842ebfdc86babbdb99dc1f69f0b9a02dcea3
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2021-10-25 09:22:44 +0800

    :bug: make confirmationMessage nullable to avoid parse errors

--- CommonCoreLegacy/src/main/java/com/skedgo/tripkit/common/model/BookingConfirmationAction.java
+++ CommonCoreLegacy/src/main/java/com/skedgo/tripkit/common/model/BookingConfirmationAction.java
@@ -59,5 +59,5 @@ public abstract class BookingConfirmationAction implements Parcelable {
   public abstract String title();
   public abstract String type();
   @Nullable public abstract ArrayList< BookingConfirmationInput> input();
-  public abstract String confirmationMessage();
+  @Nullable public abstract String confirmationMessage();
 }
