e8cb489477e73487d9c09196d1b5acb03573f9c5
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2021-11-22 12:34:39 +0800

    set leave at arrive by values per white label

--- TripKitAndroid/src/main/java/com/skedgo/tripkit/DateTimePickerConfig.kt
+++ TripKitAndroid/src/main/java/com/skedgo/tripkit/DateTimePickerConfig.kt
@@ -1,6 +1,6 @@
 package com.skedgo.tripkit
 
 data class DateTimePickerConfig(
-        val dateTimePickerLeaveAtLabel: String = "Leave at",
-        val dateTimePickerArriveByLabel: String = "Arrive by"
+        val dateTimePickerLeaveAtLabel: String,
+        val dateTimePickerArriveByLabel: String
 )
