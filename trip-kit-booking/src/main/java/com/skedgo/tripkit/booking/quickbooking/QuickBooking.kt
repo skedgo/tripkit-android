2e8c6c18d2177f087248c99b4a669712e3021945
Michael Angelo Reyes <michaelangeloreyes.skedgo@gmail.com>
2023-05-12 07:01:29 +0000

    Merge branch 'bug/Bug-19282' into 'master'
    https://redmine.buzzhives.com/issues/19282
    
    See merge request SkedGo/Android/tripkit-android!446

--- trip-kit-booking/src/main/java/com/skedgo/tripkit/booking/quickbooking/QuickBooking.kt
+++ trip-kit-booking/src/main/java/com/skedgo/tripkit/booking/quickbooking/QuickBooking.kt
@@ -5,23 +5,23 @@ import com.skedgo.tripkit.common.model.BookingConfirmationInputOptions
 import com.skedgo.tripkit.common.model.BookingConfirmationNotes
 
 data class QuickBooking(
-        val bookingTitle: String,
-        val bookingURL: String,
-        val bookingURLIsDeepLink: Boolean,
-        val count: Int,
-        val index: Int,
-        val input: List<Input>,
-        val title: String,
-        val tripUpdateURL: String,
-        val tickets: List<Ticket>,
-        val billingEnabled: Boolean
+    val bookingTitle: String,
+    val bookingURL: String,
+    val bookingURLIsDeepLink: Boolean,
+    val count: Int,
+    val index: Int,
+    val input: List<Input>,
+    val title: String,
+    val tripUpdateURL: String,
+    val tickets: List<Ticket>,
+    val billingEnabled: Boolean
 )
 
 data class Option(
-        val id: String,
-        val title: String,
-        val timestamp: String? = null,
-        val provider: String? = null
+    val id: String,
+    val title: String,
+    val timestamp: String? = null,
+    val provider: String? = null
 ) {
     companion object {
         fun parseBookingConfirmationInputOptions(opt: BookingConfirmationInputOptions): Option {
@@ -35,43 +35,45 @@ data class Option(
 }
 
 data class Input(
-        val id: String,
-        val options: List<Option>?,
-        val required: Boolean,
-        val title: String,
-        val type: String,
-        var raw_value: String,
-        var value: String?,
-        var values: List<String>?,
-        val minValue: Int,
-        val maxValue: Int
+    val id: String,
+    val options: List<Option>?,
+    val required: Boolean,
+    val title: String,
+    val type: String,
+    var raw_value: String,
+    var value: String?,
+    var values: List<String>?,
+    val minValue: Int,
+    val maxValue: Int
 ) {
     companion object {
         fun parse(bookingConfirmationInput: BookingConfirmationInputNew): Input =
-                Input(
-                        bookingConfirmationInput.id(),
-                        bookingConfirmationInput.options().map {
-                            Option.parseBookingConfirmationInputOptions(it)
-                        },
-                        bookingConfirmationInput.required(),
-                        bookingConfirmationInput.title(),
-                        bookingConfirmationInput.type(),
-                        "",
-                        bookingConfirmationInput.value(),
-                        bookingConfirmationInput.values(),
-                        0,
-                        0
-                )
+            Input(
+                bookingConfirmationInput.id(),
+                bookingConfirmationInput.options().map {
+                    Option.parseBookingConfirmationInputOptions(it)
+                },
+                bookingConfirmationInput.required(),
+                bookingConfirmationInput.title(),
+                bookingConfirmationInput.type(),
+                "",
+                bookingConfirmationInput.value(),
+                bookingConfirmationInput.values(),
+                0,
+                0
+            )
 
     }
 
     fun setValuesFromId() {
-        if(options != null) {
+        if (options != null) {
             value = value?.run { options.firstOrNull { it.title == this }?.id }
             values = values?.run {
                 val data = mutableListOf<String>()
                 forEach { value ->
-                    data.add(options.firstOrNull { it.title == value }?.id ?: "")
+                    options.firstOrNull { it.title == value }?.id?.let {
+                        data.add(it)
+                    }
                 }
                 data
             }
@@ -91,11 +93,11 @@ data class Input(
 }
 
 data class Ticket(
-        val id: String,
-        val currency: String,
-        val description: String,
-        val name: String,
-        val price: Double,
-        var value: Long?,
-        val max: Int? = null
+    val id: String,
+    val currency: String,
+    val description: String,
+    val name: String,
+    val price: Double,
+    var value: Long?,
+    val max: Int? = null
 )
