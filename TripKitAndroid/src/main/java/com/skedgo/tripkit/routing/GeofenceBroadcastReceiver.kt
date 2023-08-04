36cda6214efcf15cd6a771e1efda2d55e2821a1b
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2023-08-04 16:51:01 +0800

    [odin_1.0.73] Playstore reject fixes

--- TripKitAndroid/src/main/java/com/skedgo/tripkit/routing/GeofenceBroadcastReceiver.kt
+++ TripKitAndroid/src/main/java/com/skedgo/tripkit/routing/GeofenceBroadcastReceiver.kt
@@ -89,7 +89,7 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
                     context,
                     0,
                     intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
 
         }
