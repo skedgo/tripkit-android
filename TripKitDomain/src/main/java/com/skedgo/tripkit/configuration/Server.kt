b399ac661b70f5d284f0236e98a84ee9213c8cf7
lenganngoh <mei1690@yahoo.com>
2023-08-22 22:33:37 +0800

    Token transit implementation

--- TripKitDomain/src/main/java/com/skedgo/tripkit/configuration/Server.kt
+++ TripKitDomain/src/main/java/com/skedgo/tripkit/configuration/Server.kt
@@ -2,7 +2,9 @@ package com.skedgo.tripkit.configuration
 
 enum class Server(val value: String) {
     ApiTripGo("https://api.tripgo.com/v1/"),
-//    ApiTripGo("https://galaxies.skedgo.com/lab/beta/satapp/"),
+
+    //    ApiTripGo("https://galaxies.skedgo.com/lab/beta/satapp/"),
     //  BigBang("https://bigbang.buzzhives.com/satapp/")
+//    BigBang("https://api-beta.tripgo.com/v1/")
     BigBang("https://galaxies.skedgo.com/lab/beta/satapp/")
 }
