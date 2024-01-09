666bd7d562ed5f7b9b156924ad79f32f481aa066
Leng Ann Goh <gohlengann@gmail.com>
2024-01-09 12:56:47 +0800

    PR comment address - addressing preferences, auth0, etc

--- TripKitAndroid/src/main/java/com/skedgo/tripkit/TripKitConstants.kt
+++ TripKitAndroid/src/main/java/com/skedgo/tripkit/TripKitConstants.kt
@@ -6,7 +6,6 @@ class TripKitConstants {
         const val PREF_NAME_TRIP_KIT = "TripKit"
         const val PREF_KEY_CLIENT_ID = "pref_key_client_id"
         const val PREF_NAME_APP = "app_preferences"
-        const val PREF_KEY_SIGNUP = "pref_sign_up"
     }
 
 }
