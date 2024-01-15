e709082fa4249e38279e61048d136de07210e043
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2024-01-15 21:08:14 +0800

    [19730] *Update TripGoSharedPreference to make it injectable and not initialized by application context to avoid memory leakage. *Create TripKitSharedPreference to centralize handling SharePreference logic for TripKit *Focus map to selected client's region

---
+++ TripKitAndroid/src/main/java/com/skedgo/tripkit/data/TripKitSharedPreference.kt
@@ -0,0 +1,46 @@
+package com.skedgo.tripkit.data
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import com.skedgo.tripkit.TripKitConstants.Companion.PREF_KEY_CLIENT_ID
+import com.skedgo.tripkit.TripKitConstants.Companion.PREF_KEY_POLYGON
+import com.skedgo.tripkit.TripKitConstants.Companion.PREF_KEY_TRIP_KIT_LAT_LNG
+import com.skedgo.tripkit.TripKitConstants.Companion.PREF_NAME_TRIP_KIT
+import com.skedgo.tripkit.account.data.Polygon
+import com.skedgo.tripkit.common.util.TripKitLatLng
+import com.skedgo.tripkit.extensions.fromJson
+import javax.inject.Inject
+
+// TODO update codes to use this for accessing TripKit SharedPreference
+/**
+ * Singleton class to centralize handling of TripKit local data using [SharedPreferences]
+ */
+@SuppressLint("StaticFieldLeak")
+class TripKitSharedPreference @Inject constructor(context: Context) : BaseSharedPreference(context) {
+
+    override val prefenceKey: String
+        get() = PREF_NAME_TRIP_KIT
+
+    fun saveClientId(clientId: String) {
+        sharedPreferences.edit()
+            .putString(PREF_KEY_CLIENT_ID, clientId)
+            .apply()
+    }
+
+    fun getClientId(): String? =
+        sharedPreferences.getString(PREF_KEY_CLIENT_ID, null)
+
+    fun savePolygon(polygon: Polygon?) {
+        sharedPreferences.edit()
+            .putString(PREF_KEY_POLYGON, polygon?.let {
+                gson.toJson(it)
+            } ?: "")
+            .apply()
+    }
+
+    fun getPolygon(): Polygon? {
+        val dataString = sharedPreferences.getString(PREF_KEY_POLYGON, "") ?: ""
+        return gson.fromJson(dataString)
+    }
+}
