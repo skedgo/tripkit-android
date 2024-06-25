4707077563c81aeccda65831420a93ae1a9f109b
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2024-06-25 21:24:50 +0800

    [21751] - [TripGov5] update AWSAuthenticationFragment to add checking from extra if needed to sign out first on open of the auth screen - [TripGov5] update safeCall to include error code - [TripGov5] add NecAccountModule, NecSettingsManager, NecUserAccountRepository and NecUserApi for Nec specific settings Customer Portal - [TripKit] update Resource and include code for error state - [TripGov5] update WLSettingsManager and update observing of user state

--- TripKitAndroid/src/main/java/com/skedgo/network/Resource.kt
+++ TripKitAndroid/src/main/java/com/skedgo/network/Resource.kt
@@ -1,11 +1,11 @@
 package com.skedgo.network
 
-data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+data class Resource<out T>(val status: Status, val data: T?, val message: String?, val code: Int? = -1) {
     companion object {
         fun <T> success(data: T): Resource<T> = Resource(status = Status.SUCCESS, data = data, message = null)
 
-        fun <T> error(data: T?, message: String): Resource<T> =
-                Resource(status = Status.ERROR, data = data, message = message)
+        fun <T> error(data: T?, message: String, code: Int): Resource<T> =
+                Resource(status = Status.ERROR, data = data, message = message, code = code)
 
         fun <T> loading(data: T?): Resource<T> = Resource(status = Status.LOADING, data = data, message = null)
     }
