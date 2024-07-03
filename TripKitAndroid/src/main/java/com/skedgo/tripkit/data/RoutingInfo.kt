0cad147aa068c077e588f2a699f2824929b32350
json <jason@skedgo.com>
2024-07-03 23:39:04 +0800

    [20256] Support for creation of routing.json file

---
+++ TripKitAndroid/src/main/java/com/skedgo/tripkit/data/RoutingInfo.kt
@@ -0,0 +1,7 @@
+package com.skedgo.tripkit.data
+
+data class RoutingInfo(
+    val url: String?,
+    val os: String?,
+    val version: String?
+)
---
+++ TripKitAndroid/src/main/java/com/skedgo/tripkit/data/RoutingJsonResult.kt
@@ -0,0 +1,6 @@
+package com.skedgo.tripkit.data
+
+data class RoutingJsonResult(
+    val fileContent: ByteArray,
+    val base64EncodedString: String
+)
