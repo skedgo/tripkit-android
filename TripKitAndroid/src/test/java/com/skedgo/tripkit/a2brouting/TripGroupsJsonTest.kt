7fb247929c7e15b12bbcd8e05b068f528a2f37fc
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2025-01-18 01:58:22 +0800

    [22902] - [TripKitAndroid] Fix TripGroupsJsonTest unit test on TripGroups parsing

--- TripKitAndroid/src/test/java/com/skedgo/tripkit/a2brouting/TripGroupsJsonTest.kt
+++ TripKitAndroid/src/test/java/com/skedgo/tripkit/a2brouting/TripGroupsJsonTest.kt
@@ -1,11 +1,27 @@
 package com.skedgo.tripkit.a2brouting
 
 import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.skedgo.tripkit.TripKitAndroidRobolectricTest
+import com.skedgo.tripkit.extensions.fromJson
+import com.skedgo.tripkit.routing.Trip
+import com.skedgo.tripkit.routing.TripGroup
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.observers.TestObserver
-import okhttp3.mockwebserver.MockResponse
+import io.reactivex.plugins.RxJavaPlugins
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -13,20 +29,42 @@ import org.junit.runner.RunWith
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import thuytrinh.mockwebserverrule.MockWebServerRule
-import com.skedgo.tripkit.routing.Trip
-import com.skedgo.tripkit.routing.TripGroup
+import java.io.File
 import java.io.IOException
 import java.lang.reflect.Type
-import java.util.Collections
-import java.util.HashMap
+import java.nio.file.Paths
+import java.util.concurrent.TimeUnit
 import kotlin.system.measureTimeMillis
-import org.assertj.core.api.Assertions.assertThat
 
 @RunWith(AndroidJUnit4::class)
 class TripGroupsJsonTest : TripKitAndroidRobolectricTest() {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val immediateScheduler: Scheduler = object : Scheduler() {
+
+        override fun createWorker() = ExecutorScheduler.ExecutorWorker { it.run() }
+
+        // This prevents errors when scheduling a delay
+        override fun scheduleDirect(run: Runnable, delay: Long, unit: TimeUnit): Disposable {
+            return super.scheduleDirect(run, 0, unit)
+        }
+
+    }
+
+    fun initRx() {
+        RxJavaPlugins.setIoSchedulerHandler { immediateScheduler }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { immediateScheduler }
+        RxAndroidPlugins.setMainThreadSchedulerHandler { immediateScheduler }
+    }
+
+    fun tearDownRx() {
+        RxJavaPlugins.reset()
+        RxAndroidPlugins.reset()
+    }
+
     private val TRIP_GROUP_TYPE: Type = object : TypeToken<List<TripGroup>>() {}.type
 
     @Rule
@@ -37,6 +75,7 @@ class TripGroupsJsonTest : TripKitAndroidRobolectricTest() {
 
     @Before
     fun before() {
+        initRx()
         val a2bRoutingApi = Retrofit.Builder()
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
@@ -51,35 +90,62 @@ class TripGroupsJsonTest : TripKitAndroidRobolectricTest() {
         )
     }
 
+    @After
+    fun after() {
+        tearDownRx()
+    }
+
     @Test
     @Throws(IOException::class)
-    fun shouldNullifyTripRawSegmentList() {
-        val mockResponse: MockResponse = MockWebServerRule.createMockResponse("/large-routing.json")
-        serverRule.server.enqueue(mockResponse)
+    fun rawSegmentListShouldNotBeEmpty() {
+        val filePath =
+            Paths.get("src", "test", "resources", "trip-groups.json").toAbsolutePath().toString()
 
-        val subscriber: TestObserver<List<TripGroup>> = api.fetchRoutesAsync(
-            listOf(serverRule.server.url("/").toString()),
+        val jsonResponse = File(filePath).readText()
+
+        // Create a mock of the API service
+        val apiMock = mockk<FailoverA2bRoutingApi>(relaxed = true)
+
+        // Mock the response of the API call using MockK
+        val path = "mocked_url"
+        every {
+            apiMock.fetchRoutesAsync(
+                any(),
+                any(),
+                any(),
+                any(),
+                any()
+            )
+        } returns Observable.just(
+            Gson().fromJson(jsonResponse)
+        )
+
+        // Call the API method in the test
+        val subscriber: TestObserver<List<TripGroup>> = apiMock.fetchRoutesAsync(
+            listOf(path),
             emptyList(),
             emptyList(),
             emptyList(),
             HashMap()
         ).test()
 
+        // Extract the response from the subscriber
         val tripGroups: List<TripGroup> = subscriber.values()[0]
         assertThat(tripGroups).isNotNull.isNotEmpty
 
-        // Should nullify `rawSegmentList`.
-        // Otherwise, gson will convert it to json and result in
-        // a very large json which is unnecessary.
+        // Verify that `rawSegmentList` is not null
         tripGroups.forEach { tripGroup ->
             val trips: List<Trip>? = tripGroup.trips
             trips?.forEach { trip ->
-                assertThat(trip.rawSegmentList).isNull()
+                assertThat(trip.rawSegmentList).isNotNull.isNotEmpty
             }
         }
 
-        // Call for sake of benchmark.
+        // Call toJson for benchmark purposes
         toJson(Gson(), tripGroups)
+
+        // Verify that the API call was made with the correct parameters
+        verify { apiMock.fetchRoutesAsync(any(), any(), any(), any(), any()) }
     }
 
     /**
